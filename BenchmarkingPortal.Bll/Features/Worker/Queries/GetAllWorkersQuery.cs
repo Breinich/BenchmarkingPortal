696faf1e4773ab69c51ad460f0c9bc258c2c719a
Breinich <bajnokvencel@edu.bme.hu>
2023-04-13 16:38:15 +0200

    Get all workers query

---
+++ BenchmarkingPortal.Bll/Features/Worker/Queries/GetAllWorkersQuery.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.Worker.Queries;
+
+public class GetAllWorkersQuery : IRequest<IEnumerable<WorkerHeader>> { }
---
+++ BenchmarkingPortal.Bll/Features/Worker/QueryHandlers/GetAllWorkersQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkingPortal.Bll.Features.Worker.Queries;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkingPortal.Bll.Features.Worker.QueryHandlers;
+
+public class GetAllWorkersQueryHandler : IRequestHandler<GetAllWorkersQuery, IEnumerable<WorkerHeader>>
+{
+    private readonly BenchmarkingDbContext _context;
+
+    public GetAllWorkersQueryHandler(BenchmarkingDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<WorkerHeader>>
+        Handle(GetAllWorkersQuery request, CancellationToken cancellationToken) =>
+        await _context.Workers.Select(w => new WorkerHeader(w)).ToListAsync(cancellationToken);
+}
