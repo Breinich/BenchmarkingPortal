fea45f57d527cc3e264e11938335e1c0d20b7592
Breinich <bajnokvencel@edu.bme.hu>
2023-04-13 15:02:57 +0200

    Get all SourceSets query

---
+++ BenchmarkingPortal.Bll/Features/SourceSet/Queries/GetAllSourceSetsQuery.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.Queries;
+
+public class GetAllSourceSetsQuery : IRequest<IEnumerable<SourceSetHeader>> { }
---
+++ BenchmarkingPortal.Bll/Features/SourceSet/QueryHandlers/GetAllSourceSetsQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkingPortal.Bll.Features.SourceSet.Queries;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.QueryHandlers;
+
+public class GetAllSourceSetsQueryHandler : IRequestHandler<GetAllSourceSetsQuery, IEnumerable<SourceSetHeader>>
+{
+    private readonly BenchmarkingDbContext _context;
+
+    public GetAllSourceSetsQueryHandler(BenchmarkingDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task<IEnumerable<SourceSetHeader>> Handle(GetAllSourceSetsQuery request,
+        CancellationToken cancellationToken) =>
+        await _context.SourceSets.Select(s => new SourceSetHeader()
+        {
+            Id = s.Id,
+            Name = s.Name,
+            Path = s.Path,
+            UploadedDate = s.UploadedDate,
+            UserId = s.UserId,
+            Version = s.Version,
+        }).ToListAsync(cancellationToken);
+}
