d4d0ab9e2921b57ee004bfbec723640224fcca38
Breinich <bajnokvencel@edu.bme.hu>
2023-04-13 16:54:50 +0200

    Update worker command

---
+++ BenchmarkingPortal.Bll/Features/Worker/CommandHandlers/UpdateWorkerCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkingPortal.Bll.Exceptions;
+using BenchmarkingPortal.Bll.Features.Worker.Commands;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.Worker.CommandHandlers;
+
+public class UpdateWorkerCommandHandler : IRequestHandler<UpdateWorkerCommand, WorkerHeader>
+{
+    private readonly BenchmarkingDbContext _context;
+
+    public UpdateWorkerCommandHandler(BenchmarkingDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task<WorkerHeader> Handle(UpdateWorkerCommand request, CancellationToken cancellationToken)
+    {
+        var worker = await _context.Workers.FindAsync(request.WorkerId, cancellationToken) ??
+                     throw new ArgumentException(new ExceptionMessage<Dal.Entities.Worker>().ObjectNotFound);
+        
+
+        var workerDto = new WorkerHeader(worker);
+        workerDto.ComputerGroupId = request.ComputerGroupId;
+
+        // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+        // Assign the worker to an other computer group
+
+        // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+        // If succeeded, update the DB
+        worker.ComputerGroupId = request.ComputerGroupId;
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return workerDto;
+    }
+}
---
+++ BenchmarkingPortal.Bll/Features/Worker/Commands/UpdateWorkerCommand.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.Worker.Commands;
+
+public class UpdateWorkerCommand : IRequest<WorkerHeader>
+{
+    public int WorkerId { get; set; }
+    public int ComputerGroupId { get; set; }
+}
