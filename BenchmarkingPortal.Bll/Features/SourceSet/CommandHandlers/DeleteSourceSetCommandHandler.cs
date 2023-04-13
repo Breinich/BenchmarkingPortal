5e2ffc69573b00496fccfac8826371a7c9b6fcde
Breinich <bajnokvencel@edu.bme.hu>
2023-04-13 15:30:34 +0200

    Delete SourceSet command

---
+++ BenchmarkingPortal.Bll/Features/SourceSet/CommandHandlers/DeleteSourceSetCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkingPortal.Bll.Exceptions;
+using BenchmarkingPortal.Bll.Features.SourceSet.Commands;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.CommandHandlers;
+
+public class DeleteSourceSetCommandHandler : IRequestHandler<DeleteSourceSetCommand>
+{
+    private readonly BenchmarkingDbContext _context;
+    private readonly UserManager<User> _userManager; 
+
+    public DeleteSourceSetCommandHandler(BenchmarkingDbContext context, UserManager<User> userManager)
+    {
+        _context = context;
+        _userManager = userManager;
+    }
+
+
+    public async Task Handle(DeleteSourceSetCommand request, CancellationToken cancellationToken)
+    {
+        var sourceSet = await _context.SourceSets.FindAsync(request.SourceSetId, cancellationToken) ??
+                        throw new ArgumentException(new ExceptionMessage<Dal.Entities.SourceSet>().ObjectNotFound);
+
+        if (sourceSet.UserId != request.UserId)
+        {
+            var user = await _userManager.FindByIdAsync(request.UserId.ToString()) ??
+                       throw new ArgumentException(new ExceptionMessage<User>().ObjectNotFound);
+
+            var admin = await _userManager.IsInRoleAsync(user, Roles.Admin);
+
+            if (!admin)
+            {
+                throw new ArgumentException(new ExceptionMessage<Dal.Entities.SourceSet>().NoPrivilege);
+            }
+        }
+
+        _context.Remove(sourceSet);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ BenchmarkingPortal.Bll/Features/SourceSet/Commands/DeleteSourceSetCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.Commands;
+
+public class DeleteSourceSetCommand : IRequest
+{
+    public int SourceSetId { get; set; }
+    public int UserId { get; set; }
+}
