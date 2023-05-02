4b84032ff8320bfc366239828e48e49fef8d23aa
Breinich <bajnokvencel@edu.bme.hu>
2023-05-02 04:10:25 +0200

    add oauth option to Program.cs

--- BenchmarkingPortal.Web/Program.cs
+++ BenchmarkingPortal.Web/Program.cs
@@ -3,7 +3,9 @@ using BenchmarkingPortal.Dal.Entities;
 using BenchmarkingPortal.Dal.SeedInterfaces;
 using BenchmarkingPortal.Dal.SeedService;
 using BenchmarkingPortal.Web.Hosting;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Execution;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-builder.Services.AddIdentity<User, IdentityRole<int>>( options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentity<User, IdentityRole<int>>( options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<BenchmarkingDbContext>()
     .AddDefaultTokenProviders();
 
@@ -24,24 +26,31 @@ builder.Services.AddScoped<IUserSeedService, UserSeedService>();
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerDocument(o => o.Title = "Benchmarking API");
+builder.Services.AddAuthentication().AddCookie()
+    .AddGitHub(options =>
+    {
+        options.ClientId = builder.Configuration["GitHub:ClientId"] ?? throw new InvalidOperationException();
+        options.ClientSecret = builder.Configuration["GitHub:ClientSecret"] ?? throw new InvalidOperationException();
+        options.CallbackPath = "/github-oauth";
+        options.Scope.Add("read:user");
+        options.Scope.Add("user:email");
+    });
 
 var app = builder.Build();
 
 await app.MigrateDatabaseAsync<BenchmarkingDbContext>();
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseOpenApi();
-    app.UseSwaggerUi3();
-}
 
 app.UseAuthentication();
 app.UseAuthorization();
