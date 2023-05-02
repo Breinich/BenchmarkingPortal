e524ac74b7ff0d31cdb7a77d693c0b31e873bd2e
Breinich <bajnokvencel@edu.bme.hu>
2023-05-02 04:15:47 +0200

    clone migrations to the Test migrations

---
+++ BenchmarkingPortal.Migrations.Test/20230410183555_InitialwConfigurations.cs
@@ -0,0 +1,22 @@
+﻿#nullable disable
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BenchmarkingPortal.Migrations.Test
+{
+    /// <inheritdoc />
+    public partial class InitialwConfigurations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
