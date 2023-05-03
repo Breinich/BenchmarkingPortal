52d7e1789e580acd831529505dc0792ec5eb74a6
Breinich <bajnokvencel@edu.bme.hu>
2023-05-03 20:28:25 +0200

    add role seeding and policy setups

--- BenchmarkingPortal.Dal/SeedService/RoleSeedService.cs
+++ BenchmarkingPortal.Dal/SeedService/RoleSeedService.cs
@@ -16,5 +16,9 @@ public class RoleSeedService :IRoleSeedService
     {
         if (!await _roleManager.RoleExistsAsync(Roles.Admin))
             await _roleManager.CreateAsync(new IdentityRole<int> { Name = Roles.Admin });
+        if (!await _roleManager.RoleExistsAsync(Roles.User))
+            await _roleManager.CreateAsync(new IdentityRole<int> { Name = Roles.User });
+        if (!await _roleManager.RoleExistsAsync(Roles.Guest))
+            await _roleManager.CreateAsync(new IdentityRole<int> { Name = Roles.Guest });
     }
 }
--- BenchmarkingPortal.Web/Program.cs
+++ BenchmarkingPortal.Web/Program.cs
@@ -1,3 +1,4 @@
+using BenchmarkingPortal;
 using BenchmarkingPortal.Dal;
 using BenchmarkingPortal.Dal.Entities;
 using BenchmarkingPortal.Dal.SeedInterfaces;
@@ -17,6 +18,25 @@ builder.Services.AddIdentity<User, IdentityRole<int>>( options => options.SignIn
     .AddEntityFrameworkStores<BenchmarkingDbContext>()
     .AddDefaultTokenProviders();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+// Password settings
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+// Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+// User settings
+    options.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
 builder.Services.AddDbContext<BenchmarkingDbContext>(
     o => o.UseSqlServer(builder.Configuration.GetConnectionString(nameof(BenchmarkingDbContext)),
         x => x.MigrationsAssembly("BenchmarkingPortal.Migrations.Base")));
@@ -34,7 +54,28 @@ builder.Services.AddAuthentication().AddCookie()
         options.CallbackPath = "/github-oauth";
         options.Scope.Add("read:user");
         options.Scope.Add("user:email");
+        options.Scope.Add("read:org");
     });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAdministratorRole",
+        policy => policy.RequireRole(Roles.Admin));
+    options.AddPolicy("RequireApprovedUser",
+        policy => policy.RequireRole(Roles.User, Roles.Admin));
+});
+
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+// Cookie settings
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.SlidingExpiration = true;
+});
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
@@ -45,6 +86,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
