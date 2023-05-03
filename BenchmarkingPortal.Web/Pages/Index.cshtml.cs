4cab19aa5426681688a2c943f345b7f61ef83e02
Breinich <bajnokvencel@edu.bme.hu>
2023-05-03 15:30:08 +0200

    "/" routing update

--- BenchmarkingPortal.Web/Pages/Index.cshtml.cs
+++ BenchmarkingPortal.Web/Pages/Index.cshtml.cs
@@ -1,19 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using BenchmarkingPortal.Dal.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BenchmarkingPortal.Web.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly SignInManager<User> _signInManager;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, SignInManager<User> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
 
         public void OnGet()
         {
-            
+            Response.Redirect(_signInManager.IsSignedIn(User) ? "/Home" : "/Identity/Account/Login");
         }
     }
 }
