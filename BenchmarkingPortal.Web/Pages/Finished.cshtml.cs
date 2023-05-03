22771f2fd377e9ac3f128ad6d1cc8caf7aba4ada
Breinich <bajnokvencel@edu.bme.hu>
2023-05-03 16:18:56 +0200

    User role check

--- BenchmarkingPortal.Web/Pages/Finished.cshtml.cs
+++ BenchmarkingPortal.Web/Pages/Finished.cshtml.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using BenchmarkingPortal.Dal.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BenchmarkingPortal.Web.Pages;
 
 public class Finished : PageModel
 {
+    private readonly SignInManager<User> _signInManager;
+
+    public Finished(SignInManager<User> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+    
     public void OnGet()
     {
-        
+        if(!_signInManager.IsSignedIn(User))
+        {
+            Response.Redirect("/Identity/Account/Login");
+        }
+        else if(User.IsInRole(Roles.Guest))
+        {
+            Response.Redirect("/Identity/Account/AccessDenied");
+        }
     }
 }
--- BenchmarkingPortal.Web/Pages/Home.cshtml.cs
+++ BenchmarkingPortal.Web/Pages/Home.cshtml.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using BenchmarkingPortal.Dal.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BenchmarkingPortal.Web.Pages;
 
 public class Home : PageModel
 {
+    private readonly SignInManager<User> _signInManager;
+    public Home(SignInManager<User> signInManager)
+    {
+        _signInManager = signInManager;
+    }
     public void OnGet()
     {
-        
+        if(!_signInManager.IsSignedIn(User))
+        {
+            Response.Redirect("/Identity/Account/Login");
+        }
+        else if(User.IsInRole(Roles.Guest))
+        {
+            Response.Redirect("/Identity/Account/AccessDenied");
+        }
     }
 }
