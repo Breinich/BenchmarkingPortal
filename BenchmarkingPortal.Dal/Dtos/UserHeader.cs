79463e656f7992c335b57627951cd8c156e7f8dd
Breinich <bajnokvencel@edu.bme.hu>
2023-05-04 19:29:33 +0200

    UserHeader update for purpose

--- BenchmarkingPortal.Dal/Dtos/UserHeader.cs
+++ BenchmarkingPortal.Dal/Dtos/UserHeader.cs
@@ -3,12 +3,10 @@
 public class UserHeader
 {
     public int Id { get; set; }
-
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+    public string? Role { get; set; }
+    
     public bool Subscription { get; set; }
     
-    public string? GitHubUserName { get; set; }
-
-    public string? UserName { get; set; }
-
-    public string? Email { get; set; }
 }
