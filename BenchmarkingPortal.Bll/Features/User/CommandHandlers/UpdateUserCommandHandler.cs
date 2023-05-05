4019e522ce5b580d174a0a5fd5c05ddb00a267c0
Breinich <bajnokvencel@edu.bme.hu>
2023-05-05 19:54:53 +0200

    add UpdateUserCommand

---
+++ BenchmarkingPortal.Bll/Features/User/CommandHandlers/UpdateUserCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkingPortal.Bll.Exceptions;
+using BenchmarkingPortal.Bll.Features.User.Commands;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace BenchmarkingPortal.Bll.Features.User.CommandHandlers;
+
+public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, UserHeader>
+{
+    private readonly UserManager<Dal.Entities.User> _usermanager;
+
+    public UpdateUserCommandHandler(UserManager<Dal.Entities.User> userManager)
+    {
+        _usermanager = userManager;
+    }
+
+
+    public async Task<UserHeader> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        var invoker = await _usermanager.FindByNameAsync(request.InvokerName) ??
+                      throw new ArgumentException(new ExceptionMessage<Dal.Entities.User>().ObjectNotFound);
+
+        if (await _usermanager.IsInRoleAsync(invoker, Roles.Admin))
+        {
+            var userEntity = await _usermanager.FindByNameAsync(request.UserName) ??
+                             throw new ArgumentException(new ExceptionMessage<Dal.Entities.User>().ObjectNotFound);
+
+            userEntity.Subscription = request.Subscription;
+            await _usermanager.UpdateAsync(userEntity);
+            
+            var userHeader = new UserHeader(userEntity);
+
+            var currentRoles = await _usermanager.GetRolesAsync(userEntity);
+            if(request.Role != null && !currentRoles.Contains(request.Role))
+            {
+                await _usermanager.RemoveFromRolesAsync(userEntity, currentRoles);
+                await _usermanager.AddToRoleAsync(userEntity, request.Role);
+                userHeader.Roles.Add(request.Role);
+            }
+
+            return userHeader;
+        }
+        else
+        {
+            throw new ArgumentException(new ExceptionMessage<Dal.Entities.User>().NoPrivilege);
+        }
+    }
+}
---
+++ BenchmarkingPortal.Bll/Features/User/Commands/UpdateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.User.Commands;
+
+public class UpdateUserCommand : IRequest<UserHeader>
+{
+    public string InvokerName { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+    public bool Subscription { get; set; }
+    public string? Role { get; set; }
+}
