7b5f58778c8dfb75a42eb02adb54f83f77a91ca2
Breinich <bajnokvencel@edu.bme.hu>
2023-05-07 20:33:08 +0200

    Finished page beginning

--- BenchmarkingPortal.Web/Pages/Finished.cshtml.cs
+++ BenchmarkingPortal.Web/Pages/Finished.cshtml.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BenchmarkingPortal.Bll.Features.Benchmark.Commands;
+using BenchmarkingPortal.Bll.Features.Benchmark.Queries;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BenchmarkingPortal.Web.Pages;
@@ -6,9 +11,62 @@ namespace BenchmarkingPortal.Web.Pages;
 [Authorize(Policy = Policies.RequireApprovedUser)]
 public class Finished : PageModel
 {
-   
-    public void OnGet()
-    {
+    private readonly IMediator _mediator;
 
+    [BindProperty]
+     public string? StatusMessage { get; set; }
+     [BindProperty]
+     public List<BenchmarkHeader> FinishedBenchmarks { get; set; }
+    
+     
+     
+     public Finished(IMediator mediator)
+     {
+         _mediator = mediator;
+         FinishedBenchmarks = new List<BenchmarkHeader>();
+     }
+     
+    public async Task<IActionResult> OnGet()
+    {
+        try
+        {
+            FinishedBenchmarks = (await _mediator.Send(new GetAllBenchmarksQuery()
+            {
+                Finished = true
+            })).ToList();
+
+            return Page();
+        }
+        catch (AggregateException e)
+        {
+            Console.WriteLine(e);
+            StatusMessage = "Error: " +e.InnerException?.Message;
+
+            return RedirectToPage();
+        }
     }
+    
+    public async Task<IActionResult> OnPostDeleteAsync(int id)
+    {
+        try
+        {
+            await _mediator.Send(new DeleteBenchmarkCommand()
+            {
+                Id = id,
+                InvokerName = User.Identity?.Name ?? throw new InvalidOperationException()
+            });
+
+            StatusMessage = "Benchmark deleted successfully.";
+            return RedirectToPage();
+        }
+        catch (AggregateException e)
+        {
+            Console.WriteLine(e);
+            StatusMessage = "Error: " +e.InnerException?.Message;
+
+            return RedirectToPage();
+        }
+    }
+
+  
 }
