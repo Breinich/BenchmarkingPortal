56296f1efbe25f4b5a858fad207d708e651f1b73
Breinich <bajnokvencel@edu.bme.hu>
2023-05-16 20:47:37 +0200

    migrations for the tests

---
+++ BenchmarkingPortal.Migrations.Test/20230516181608_RemoveUserId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BenchmarkingPortal.Migrations.Base.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Benchmarks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Benchmarks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
