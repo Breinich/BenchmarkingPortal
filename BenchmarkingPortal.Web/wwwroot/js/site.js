3648262474e3473f5a2823f1be2e5eeda551870e
Breinich <bajnokvencel@edu.bme.hu>
2023-05-17 20:04:30 +0200

    editComputerGroup modal filler

--- BenchmarkingPortal.Web/wwwroot/js/site.js
+++ BenchmarkingPortal.Web/wwwroot/js/site.js
@@ -173,3 +173,19 @@ function deleteSession() {
         }
     });
 }
+
+const editComputerGroupModal = document.getElementById('editCG');
+if(editComputerGroupModal){
+    editComputerGroupModal.addEventListener('show.bs.modal', function (event) {
+        const button = event.relatedTarget;
+        const computerGroupId = button.getAttribute('data-bs-id');
+        const computerGroupDesc = button.getAttribute('data-bs-desc');
+        const modalTitle = editComputerGroupModal.querySelector('.modal-title');
+        const modalBodyInput = editComputerGroupModal.querySelector('.modal-body #editDesc');
+        const modalSaveButton = editComputerGroupModal.querySelector('.modal-footer #saveEdit');
+
+        modalTitle.textContent = 'Edit Computer Group: ' + computerGroupId;
+        modalBodyInput.value = computerGroupDesc;
+        modalSaveButton.setAttribute('asp-route-id', computerGroupId);
+    });
+}
