c7e788814787257b437605be86feebb6be254dd7
Breinich <bajnokvencel@edu.bme.hu>
2023-05-17 16:58:28 +0200

    GetAllComputerGroupsWithStatsQuery

---
+++ BenchmarkingPortal.Bll/Features/ComputerGroup/Queries/GetAllComputerGroupsWithStatsQuery.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.ComputerGroup.Queries;
+
+public class GetAllComputerGroupsWithStatsQuery : IRequest<IEnumerable<ComputerGroupHeader>> { }
---
+++ BenchmarkingPortal.Bll/Features/ComputerGroup/QueryHandlers/GetAllComputerGroupsWithStatsQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkingPortal.Bll.Features.ComputerGroup.Queries;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkingPortal.Bll.Features.ComputerGroup.QueryHandlers;
+
+public class GetAllComputerGroupsWithStatsQueryHandler : IRequestHandler<GetAllComputerGroupsWithStatsQuery, IEnumerable<ComputerGroupHeader>>
+{
+    private readonly BenchmarkingDbContext _context;
+
+    public GetAllComputerGroupsWithStatsQueryHandler(BenchmarkingDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task<IEnumerable<ComputerGroupHeader>> Handle(GetAllComputerGroupsWithStatsQuery request, CancellationToken cancellationToken)
+    {
+        var computerGroups = await _context.ComputerGroups
+            .Include(x => x.Workers)
+            .Include(x => x.Benchmarks)
+            .Select(x => new ComputerGroupHeader(x)
+            {
+                BenchmarkCount = x.Benchmarks.Count,
+                WorkerCount = x.Workers.Count
+            })
+            .ToListAsync(cancellationToken);
+        
+        return computerGroups;
+    }
+}
