90530c02bfa5536667248088f0679420c63abb47
Breinich <bajnokvencel@edu.bme.hu>
2023-05-18 02:35:00 +0200

    fix handling configuration's nullable properties

--- BenchmarkingPortal.Bll/Features/Configuration/CommandHandlers/CreateConfigurationCommandHandler.cs
+++ BenchmarkingPortal.Bll/Features/Configuration/CommandHandlers/CreateConfigurationCommandHandler.cs
@@ -19,29 +19,35 @@ public class CreateConfigurationCommandHandler : IRequestHandler<CreateConfigura
     public async Task<ConfigurationHeader> Handle(CreateConfigurationCommand request,
         CancellationToken cancellationToken)
     {
+        // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        // config check needed based on the constraints
+        // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        
         var config = new Dal.Entities.Configuration();
 
         await _context.Configurations.AddAsync(config, cancellationToken);
         await _context.SaveChangesAsync(cancellationToken);
 
-        foreach (var configItem in request.Configurations)
-            await _context.ConfigurationItems.AddAsync(new ConfigurationItem
-            {
-                Key = configItem.Item2,
-                Value = configItem.Item3,
-                Scope = configItem.Item1,
-                ConfigurationId = config.Id
-            }, cancellationToken);
+        if (request.Configurations != null)
+            foreach (var configItem in request.Configurations)
+                await _context.ConfigurationItems.AddAsync(new ConfigurationItem
+                {
+                    Key = configItem.Item2,
+                    Value = configItem.Item3,
+                    Scope = configItem.Item1,
+                    ConfigurationId = config.Id
+                }, cancellationToken);
 
         await _context.SaveChangesAsync(cancellationToken);
 
-        foreach (var constraint in request.Constraints)
-            await _context.Constraints.AddAsync(new Constraint
-            {
-                Premise = constraint.Item1,
-                Consequence = constraint.Item2,
-                ConfigurationId = config.Id
-            }, cancellationToken);
+        if (request.Constraints != null)
+            foreach (var constraint in request.Constraints)
+                await _context.Constraints.AddAsync(new Constraint
+                {
+                    Premise = constraint.Item1,
+                    Consequence = constraint.Item2,
+                    ConfigurationId = config.Id
+                }, cancellationToken);
 
         await _context.SaveChangesAsync(cancellationToken);
 
--- BenchmarkingPortal.Bll/Features/Configuration/Commands/CreateConfigurationCommand.cs
+++ BenchmarkingPortal.Bll/Features/Configuration/Commands/CreateConfigurationCommand.cs
@@ -6,6 +6,6 @@ namespace BenchmarkingPortal.Bll.Features.Configuration.Commands;
 
 public class CreateConfigurationCommand : IRequest<ConfigurationHeader>
 {
-    public List<(Scope, string, string)> Configurations { get; set; } = null!;
-    public List<(string, string)> Constraints { get; set; } = null!;
+    public List<(Scope, string, string)>? Configurations { get; set; }
+    public List<(string, string)>? Constraints { get; set; }
 }
