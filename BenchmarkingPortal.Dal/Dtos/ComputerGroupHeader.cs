06c2fe78fca5cd3f3ff668e03e36c9a60339700d
Breinich <bajnokvencel@edu.bme.hu>
2023-05-28 04:01:08 +0200

    dto cleanup

--- BenchmarkingPortal.Dal/Dtos/ComputerGroupHeader.cs
+++ BenchmarkingPortal.Dal/Dtos/ComputerGroupHeader.cs
@@ -17,8 +17,8 @@ public class ComputerGroupHeader
     public int Id { get; set; }
 
     public string? Description { get; set; }
-    
+
     public int WorkerCount { get; set; }
-    
+
     public int BenchmarkCount { get; set; }
 }
