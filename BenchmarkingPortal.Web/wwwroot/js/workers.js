1ae14a105ea8fd4cac5029ec372250db13b763d0
Breinich <vencel@bajnok.hu>
2023-10-14 16:03:37 +0200

    📦 chore(workers.js): add workers.js file to handle editing computer groups in the frontend
    🐛 fix(workers.js): add newline at the end of the file to adhere to coding conventions

---
+++ BenchmarkingPortal.Web/wwwroot/js/workers.js
@@ -0,0 +1,15 @@
+﻿const editComputerGroupModal = document.getElementById('editCG');
+if (editComputerGroupModal) {
+    editComputerGroupModal.addEventListener('show.bs.modal', function (event) {
+        const button = event.relatedTarget;
+        const computerGroupId = button.getAttribute('data-bs-id');
+        const computerGroupDesc = button.getAttribute('data-bs-desc');
+        const modalTitle = editComputerGroupModal.querySelector('.modal-title');
+        const modalBodyInput = editComputerGroupModal.querySelector('.modal-body #editDesc');
+        const modalEditId = editComputerGroupModal.querySelector('.modal-body #editId');
+
+        modalTitle.textContent = 'Edit Computer Group: ' + computerGroupId;
+        modalBodyInput.value = computerGroupDesc;
+        modalEditId.value = computerGroupId;
+    });
+}
