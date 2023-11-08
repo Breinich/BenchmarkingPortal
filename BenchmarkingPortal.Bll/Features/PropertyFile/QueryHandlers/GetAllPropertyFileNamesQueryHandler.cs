e1ec5c92ff55be0afd047d46bce0e77ec3e7ec0b
Breinich <vencel@bajnok.hu>
2023-11-08 14:32:39 +0100

    🔧 chore(BenchmarkingPortal.Bll.csproj): remove unused folders from the project file to improve organization and maintainability 🔧 chore(GetAllPropertyFileNamesQueryHandler.cs): rename GetPropertyFileNamesQueryHandler to GetAllPropertyFileNamesQueryHandler for consistency and clarity 🔧 chore(GetAllSetFileNamesQueryHandler.cs): add missing using statement for Microsoft.Extensions.Configuration and implement missing logic to retrieve all set file names from the configured SV-Benchmarks path

--- BenchmarkingPortal.Bll/Features/PropertyFile/QueryHandlers/GetAllPropertyFileNamesQueryHandler.cs
+++ BenchmarkingPortal.Bll/Features/PropertyFile/QueryHandlers/GetAllPropertyFileNamesQueryHandler.cs
@@ -4,16 +4,16 @@ using Microsoft.Extensions.Configuration;
 
 namespace BenchmarkingPortal.Bll.Features.PropertyFile.QueryHandlers;
 
-public class GetPropertyFileNamesQueryHandler : IRequestHandler<GetPropertyFileNamesQuery, IEnumerable<string>>
+public class GetAllPropertyFileNamesQueryHandler : IRequestHandler<GetAllPropertyFileNamesQuery, IEnumerable<string>>
 {
     private readonly IConfiguration _configuration;
     
-    public GetPropertyFileNamesQueryHandler(IConfiguration configuration)
+    public GetAllPropertyFileNamesQueryHandler(IConfiguration configuration)
     {
         _configuration = configuration;
     }
     
-    public Task<IEnumerable<string>> Handle(GetPropertyFileNamesQuery request, CancellationToken cancellationToken)
+    public Task<IEnumerable<string>> Handle(GetAllPropertyFileNamesQuery request, CancellationToken cancellationToken)
     {
         return Task.FromResult(Directory.GetFiles((_configuration["Storage:SV-Benchmarks"] ??
                             throw new ApplicationException("SV-Benchmarks path is not configured."))
--- BenchmarkingPortal.Bll/Features/SetFile/QueryHandlers/GetAllSetFileNamesQueryHandler.cs
+++ BenchmarkingPortal.Bll/Features/SetFile/QueryHandlers/GetAllSetFileNamesQueryHandler.cs
@@ -1,12 +1,23 @@
 ﻿using BenchmarkingPortal.Bll.Features.SetFile.Queries;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 
 namespace BenchmarkingPortal.Bll.Features.SetFile.QueryHandlers;
 
 public class GetAllSetFileNamesQueryHandler : IRequestHandler<GetAllSetFileNamesQuery, IEnumerable<String>>
 {
+    private readonly IConfiguration _configuration;
+    
+    public GetAllSetFileNamesQueryHandler(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
     public Task<IEnumerable<string>> Handle(GetAllSetFileNamesQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return Task.FromResult(Directory.GetFiles((_configuration["Storage:SV-Benchmarks"] ??
+                            throw new ApplicationException("SV-Benchmarks path is not configured."))
+                           + Path.DirectorySeparatorChar + "c", "*.set", SearchOption.TopDirectoryOnly)
+            .Select(s => s));
     }
 }
