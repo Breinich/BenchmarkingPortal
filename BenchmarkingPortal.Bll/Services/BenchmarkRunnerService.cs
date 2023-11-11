ad856b29d05582609218d00b6ed57ac3e160935b
Breinich <vencel@bajnok.hu>
2023-11-11 05:08:25 +0100

    📦 chore(BenchmarkRunnerService.cs): add BenchmarkRunnerService class to handle benchmark running logic 📦 chore(BenchmarkTask.cs): add BenchmarkTask class to handle individual benchmark execution and status update

---
+++ BenchmarkingPortal.Bll/Services/BenchmarkRunnerService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BenchmarkingPortal.Bll.Services;
+
+public class BenchmarkRunnerService : BackgroundService
+{
+    private IBenchmarkQueue Benchmarks { get; }
+    private ILogger<BenchmarkRunnerService> Logger { get; }
+    
+    public BenchmarkRunnerService(IBenchmarkQueue queue, ILogger<BenchmarkRunnerService> logger)
+    {
+        Benchmarks = queue;
+        Logger = logger;
+    }
+    
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using PeriodicTimer timer = new(TimeSpan.FromSeconds(1));
+        try
+        {
+            while (await timer.WaitForNextTickAsync(stoppingToken))
+            {
+                var task = await Benchmarks.DequeueAsync(stoppingToken);
+                
+                _ = Task.Run(async () => await task.ExecuteAsync(stoppingToken), stoppingToken);
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            Logger.Log(LogLevel.Information, "The benchmark runner service was stopped.");
+        }
+    }
+}
---
+++ BenchmarkingPortal.Bll/Services/BenchmarkTask.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using BenchmarkingPortal.Dal.Entities;
+using CliWrap;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BenchmarkingPortal.Bll.Services;
+
+public class BenchmarkTask
+{
+    private BenchmarkHeader BenchmarkInfo { get; }
+    private Command RunConfig { get; }
+    private readonly BenchmarkingDbContext _context;
+    private readonly ILogger<BenchmarkTask> _logger;
+    
+    public BenchmarkTask(BenchmarkHeader benchmarkInfo, Command runConfig, BenchmarkingDbContext context, ILogger<BenchmarkTask> logger)
+    {
+        BenchmarkInfo = benchmarkInfo;
+        RunConfig = runConfig;
+        _context = context;
+        _logger = logger;
+    }
+    
+    /// <summary>
+    /// Run the benchmark and update the status in the database.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    public async ValueTask ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var result = await RunConfig.ExecuteAsync(cancellationToken);
+        
+        var benchmark = await _context.Benchmarks.Where(b => b.Name == BenchmarkInfo.Name).FirstAsync(cancellationToken);
+        benchmark.Status = Status.Finished;
+        
+        if (result.ExitCode != 0)
+        {
+            _logger.Log(LogLevel.Warning, $"The benchmark {BenchmarkInfo.Name} did not finish successfully.\n" +
+                                          "You can check the errors in the error.log file among the result files.");
+        }
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
