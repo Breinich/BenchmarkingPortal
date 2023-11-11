a1742e3e0833f738e095a3a69d5772dad306f24d
Breinich <vencel@bajnok.hu>
2023-11-11 05:07:32 +0100

    🔀 refactor(StoragePaths.cs): rename namespace from BenchmarkingPortal.Web to BenchmarkingPortal.Bll.Services to match file location and improve code organization 🔀 refactor(StoragePaths.cs): delete unused StoragePaths.cs file to remove redundancy and improve code cleanliness 🆕 feat(StoragePaths.cs): create new StoragePaths class in BenchmarkingPortal.Bll.Services namespace to encapsulate storage paths and improve code modularity

---
+++ BenchmarkingPortal.Bll/Services/StoragePaths.cs
@@ -0,0 +1,11 @@
+﻿namespace BenchmarkingPortal.Bll.Services;
+
+public class StoragePaths
+{
+    public string WorkingDir { get; set; } = null!;
+    public string SetFilesDir { get; set; } = null!;
+    public string PropertyFilesDir { get; set; } = null!;
+    public string VcloudBenchmarkPath { get; set; } = null!;
+    public string WorkerConfig { get; set; } = null!;
+    public string SshConfig { get; set; } = null!;
+}
