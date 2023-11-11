8a78ffc503c15813db8a6fafe75fe9ad387af6a9
Breinich <vencel@bajnok.hu>
2023-11-11 05:07:58 +0100

    📦 feat(BenchmarkQueue.cs): add BenchmarkQueue class to handle queuing and dequeuing of benchmark tasks 📦 feat(IBenchmarkQueue.cs): add IBenchmarkQueue interface to define the contract for benchmark queue functionality

---
+++ BenchmarkingPortal.Bll/Services/IBenchmarkQueue.cs
@@ -0,0 +1,8 @@
+﻿namespace BenchmarkingPortal.Bll.Services;
+
+public interface IBenchmarkQueue
+{
+    public ValueTask QueueBenchmarkAsync(BenchmarkTask task, CancellationToken cancellationToken);
+    
+    public ValueTask<BenchmarkTask> DequeueAsync(CancellationToken cancellationToken);
+}
