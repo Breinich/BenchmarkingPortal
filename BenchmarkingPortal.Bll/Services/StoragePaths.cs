792bb1485831a27d02294ed25efe62028c305116
Breinich <vencel@bajnok.hu>
2023-11-11 05:51:09 +0100

    🐛 fix(ExceptionMessage.cs): initialize ObjectNotFound and NoPrivilege properties to improve code readability and maintainability
    🐛 fix(StoragePaths.cs): change property initialization to use init accessor instead of set accessor for better immutability and safety
    
    ✨ feat(docker-compose.dcproj): add qodana.sarif.json file to the project for further analysis and integration with Qodana static code analysis tool

--- BenchmarkingPortal.Bll/Services/StoragePaths.cs
+++ BenchmarkingPortal.Bll/Services/StoragePaths.cs
@@ -2,10 +2,10 @@
 
 public class StoragePaths
 {
-    public string WorkingDir { get; set; } = null!;
-    public string SetFilesDir { get; set; } = null!;
-    public string PropertyFilesDir { get; set; } = null!;
-    public string VcloudBenchmarkPath { get; set; } = null!;
+    public string WorkingDir { get; init; } = null!;
+    public string SetFilesDir { get; init; } = null!;
+    public string PropertyFilesDir { get; init; } = null!;
+    public string VcloudBenchmarkPath { get; init; } = null!;
     public string WorkerConfig { get; set; } = null!;
     public string SshConfig { get; set; } = null!;
 }
