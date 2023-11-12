7433b4f117e5a0f40e70135e99a5ab4cb99f3ad9
Breinich <vencel@bajnok.hu>
2023-11-12 06:11:37 +0100

    📦 feat(GetExecutableNameByIdQuery.cs): add GetExecutableNameByIdQuery class to retrieve the name of an executable by its ID 📦 feat(GetExecutableNameByIdQueryHandler.cs): add GetExecutableNameByIdQueryHandler class to handle GetExecutableNameByIdQuery and retrieve the name of an executable from the database

---
+++ BenchmarkingPortal.Bll/Features/Executable/Queries/GetExecutableNameByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.Executable.Queries;
+
+public class GetExecutableNameByIdQuery : IRequest<string?>
+{
+    public int Id { get; init; }
+}
---
+++ BenchmarkingPortal.Bll/Features/Executable/QueryHandlers/GetExecutableNameByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkingPortal.Bll.Features.Executable.Queries;
+using BenchmarkingPortal.Dal;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkingPortal.Bll.Features.Executable.QueryHandlers;
+
+public class GetExecutableNameByIdQueryHandler : IRequestHandler<GetExecutableNameByIdQuery, string?>
+{
+    private readonly BenchmarkingDbContext _context;
+    
+    public GetExecutableNameByIdQueryHandler(BenchmarkingDbContext context)
+    {
+        _context = context;
+    }
+    
+    public Task<string?> Handle(GetExecutableNameByIdQuery request, CancellationToken cancellationToken)
+    {
+        return _context.Executables.Where(e => e.Id == request.Id).Select(e => e.Name)
+            .FirstOrDefaultAsync(cancellationToken);
+    }
+}
