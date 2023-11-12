d51b3805db06bb085bef6228e89dd63509ac9eba
Breinich <vencel@bajnok.hu>
2023-11-12 06:12:36 +0100

    🔧 fix(BenchmarkRunnerService.cs): change namespace import from Microsoft.Extensions.Hosting to Microsoft.Extensions.DependencyInjection to resolve compilation error ✨ feat(BenchmarkRunnerService.cs): add IServiceScopeFactory parameter to BenchmarkRunnerService constructor to enable resolving scoped services 🔧 fix(BenchmarkRunnerService.cs): change Task.Run argument to pass IServiceScopeFactory to task.ExecuteAsync method 🔧 fix(BenchmarkTask.cs): remove BenchmarkingDbContext and ILogger dependencies from BenchmarkTask constructor ✨ feat(BenchmarkTask.cs): add IServiceScopeFactory parameter to ExecuteAsync method to enable resolving scoped services 🔧 fix(BenchmarkTask.cs): use IServiceScopeFactory to resolve BenchmarkingDbContext and ILogger in ExecuteAsync method ✨ feat(BenchmarkTask.cs): add logging for successful benchmark execution 🔧 fix(BenchmarkTask.cs): add exception handling and logging for errors that occur during task execution

--- BenchmarkingPortal.Bll/Services/BenchmarkRunnerService.cs
+++ BenchmarkingPortal.Bll/Services/BenchmarkRunnerService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace BenchmarkingPortal.Bll.Services;
@@ -7,11 +8,13 @@ public class BenchmarkRunnerService : BackgroundService
 {
     private IBenchmarkQueue Benchmarks { get; }
     private ILogger<BenchmarkRunnerService> Logger { get; }
+    private IServiceScopeFactory ScopeFactory { get; }
     
-    public BenchmarkRunnerService(IBenchmarkQueue queue, ILogger<BenchmarkRunnerService> logger)
+    public BenchmarkRunnerService(IBenchmarkQueue queue, ILogger<BenchmarkRunnerService> logger, IServiceScopeFactory scopeFactory)
     {
         Benchmarks = queue;
         Logger = logger;
+        ScopeFactory = scopeFactory;
     }
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -23,7 +26,7 @@ public class BenchmarkRunnerService : BackgroundService
             {
                 var task = await Benchmarks.DequeueAsync(stoppingToken);
                 
-                _ = Task.Run(async () => await task.ExecuteAsync(stoppingToken), stoppingToken);
+                _ = Task.Run(async () => await task.ExecuteAsync(ScopeFactory, stoppingToken), stoppingToken);
             }
         }
         catch (OperationCanceledException)
--- BenchmarkingPortal.Bll/Services/BenchmarkTask.cs
+++ BenchmarkingPortal.Bll/Services/BenchmarkTask.cs
@@ -3,6 +3,7 @@ using BenchmarkingPortal.Dal.Dtos;
 using BenchmarkingPortal.Dal.Entities;
 using CliWrap;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace BenchmarkingPortal.Bll.Services;
@@ -11,34 +12,47 @@ public class BenchmarkTask
 {
     private BenchmarkHeader BenchmarkInfo { get; }
     private Command RunConfig { get; }
-    private readonly BenchmarkingDbContext _context;
-    private readonly ILogger<BenchmarkTask> _logger;
-    
-    public BenchmarkTask(BenchmarkHeader benchmarkInfo, Command runConfig, BenchmarkingDbContext context, ILogger<BenchmarkTask> logger)
+    public BenchmarkTask(BenchmarkHeader benchmarkInfo, Command runConfig)
     {
         BenchmarkInfo = benchmarkInfo;
         RunConfig = runConfig;
-        _context = context;
-        _logger = logger;
     }
-    
+
     /// <summary>
     /// Run the benchmark and update the status in the database.
     /// </summary>
+    /// <param name="scopeFactory">Scope factory for resolving the DB context</param>
     /// <param name="cancellationToken"></param>
-    public async ValueTask ExecuteAsync(CancellationToken cancellationToken)
+    public async ValueTask ExecuteAsync(IServiceScopeFactory scopeFactory, CancellationToken cancellationToken)
     {
-        var result = await RunConfig.ExecuteAsync(cancellationToken);
-        
-        var benchmark = await _context.Benchmarks.Where(b => b.Name == BenchmarkInfo.Name).FirstAsync(cancellationToken);
-        benchmark.Status = Status.Finished;
-        
-        if (result.ExitCode != 0)
+        if (scopeFactory == null) throw new ArgumentNullException("scopeFactory");
+        using var scope = scopeFactory.CreateScope();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<BenchmarkTask>>();
+        try
         {
-            _logger.Log(LogLevel.Warning, $"The benchmark {BenchmarkInfo.Name} did not finish successfully.\n" +
-                                          "You can check the errors in the error.log file among the result files.");
-        }
+            var context = scope.ServiceProvider.GetRequiredService<BenchmarkingDbContext>();
 
-        await _context.SaveChangesAsync(cancellationToken);
+            var result = await RunConfig.ExecuteAsync(cancellationToken);
+
+            if (result.ExitCode != 0)
+            {
+                logger.Log(LogLevel.Warning, $"The benchmark {BenchmarkInfo.Name} did not finish successfully.\n" +
+                                             "You can check the errors in the error.log file among the result files.");
+            }
+            else
+            {
+                logger.Log(LogLevel.Information, $"The benchmark {BenchmarkInfo.Name} finished successfully.");
+            }
+
+            var benchmark = await context.Benchmarks.Where(b => b.Name == BenchmarkInfo.Name)
+                .FirstAsync(cancellationToken);
+            benchmark.Status = Status.Finished;
+            await context.SaveChangesAsync(cancellationToken);
+           
+        }
+        catch (Exception ex)
+        {
+            logger.Log(LogLevel.Error, ex, "An error occurred while executing the task.");
+        }
     }
 }
