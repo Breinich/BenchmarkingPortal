676628f5723489075cfc10a1c444f24d7efd4b45
Breinich <vencel@bajnok.hu>
2023-11-16 20:08:52 +0100

    📦 chore(TusUtil.cs): add TusUtil class to handle common operations related to Tus protocol
    The TusUtil class is added to the BenchmarkingPortal.Web.Pages namespace. It includes two static methods:
    - `GetContentTypeOrDefault`: Retrieves the content type from the provided metadata dictionary. If the "contentType" key exists, it returns the corresponding value as a string. Otherwise, it returns the default value "application/octet-stream".
    - `GetContentNameOrDefault`: Retrieves the content name from the provided metadata dictionary. If the "name" key exists, it returns the corresponding value as a string. Otherwise, it returns the default value "download".

---
+++ BenchmarkingPortal.Web/Pages/TusUtil.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using tusdotnet.Models;
+
+namespace BenchmarkingPortal.Web.Pages;
+
+public class TusUtil
+{
+    public static string GetContentTypeOrDefault(Dictionary<string, Metadata> metadata, string defaultVal = "application/octet-stream")
+    {
+        return metadata.TryGetValue("contentType", out var contentType) ? 
+            contentType.GetString(Encoding.UTF8) : defaultVal;
+    }
+
+    public static string GetContentNameOrDefault(Dictionary<string, Metadata> metadata, string defaultVal = "download")
+    {
+        return metadata.TryGetValue("name", out var nameMeta) ? nameMeta.GetString(Encoding.UTF8) : defaultVal;
+    }
+}
