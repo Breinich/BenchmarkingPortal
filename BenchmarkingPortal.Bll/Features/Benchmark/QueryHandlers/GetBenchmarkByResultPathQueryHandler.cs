347d00c28d52fbfcb586bc4fecc34f93f94cb0ad
Breinich <vencel@bajnok.hu>
2023-11-16 20:06:17 +0100

    📦 feat(GetBenchmarkByResultPathQuery.cs): add new query class to retrieve benchmark by result path 📦 feat(GetBenchmarkByResultPathQueryHandler.cs): add new query handler to handle GetBenchmarkByResultPathQuery and retrieve benchmark from database by result path

---
+++ BenchmarkingPortal.Bll/Features/Benchmark/QueryHandlers/GetBenchmarkByResultPathQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkingPortal.Bll.Features.Benchmark.Queries;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkingPortal.Bll.Features.Benchmark.QueryHandlers;
+
+public class GetBenchmarkByResultPathQueryHandler : IRequestHandler<GetBenchmarkByResultPathQuery, BenchmarkHeader?>
+{
+    private readonly BenchmarkingDbContext _dbContext;
+    
+    public GetBenchmarkByResultPathQueryHandler(BenchmarkingDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<BenchmarkHeader?> Handle(GetBenchmarkByResultPathQuery request, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Benchmarks.Where(x => x.ResultPath == request.ResultPath)
+            .Select(b => new BenchmarkHeader(b)).FirstOrDefaultAsync(cancellationToken);
+    }
+}
