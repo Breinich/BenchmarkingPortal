9c189adc9bce492cbe64c228eff814a3b61709ba
Breinich <vencel@bajnok.hu>
2023-11-16 20:07:15 +0100

    📦 feat(DownloadResultCommand.cs): add new command to download result file
    The `DownloadResultCommand` class is added to the `BenchmarkingPortal.Bll.Features.Result.Commands` namespace. This command is used to request the download of a result file. It implements the `IRequest` interface and returns a tuple containing a `FileStream`, the file name, and the file extension. The command has a `Path` property to specify the path of the file to be downloaded.

---
+++ BenchmarkingPortal.Bll/Features/Result/Commands/DownloadResultCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.Result.Commands;
+
+public class DownloadResultCommand : IRequest<(FileStream, string, string)>
+{
+    public string Path { get; init; } = null!;
+}
