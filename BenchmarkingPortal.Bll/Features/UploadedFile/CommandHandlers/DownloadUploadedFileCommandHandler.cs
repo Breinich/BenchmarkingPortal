d9f1923171563a7f0efa023edd55739610f6f2d5
Breinich <vencel@bajnok.hu>
2023-11-17 02:13:16 +0100

    📦 feat(DownloadUploadedFileCommandHandler.cs): implement DownloadUploadedFileCommandHandler to handle downloading of uploaded files
    📦 feat(DownloadUploadedFileCommand.cs): implement DownloadUploadedFileCommand to represent a request to download an uploaded file

---
+++ BenchmarkingPortal.Bll/Features/UploadedFile/CommandHandlers/DownloadUploadedFileCommandHandler.cs
@@ -0,0 +1,57 @@
+﻿using BenchmarkingPortal.Bll.Features.UploadedFile.Commands;
+using BenchmarkingPortal.Bll.Services;
+using BenchmarkingPortal.Bll.Tus;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using BenchmarkingPortal.Web.Pages;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkingPortal.Bll.Features.UploadedFile.CommandHandlers;
+
+public class DownloadUploadedFileCommandHandler : IRequestHandler<DownloadUploadedFileCommand, (Stream, string, string)>
+{
+    private readonly BenchmarkingDbContext _context;
+    private readonly IMediator _mediator;
+    private readonly string _workDir;
+    private readonly string _setRoot;
+    
+    public DownloadUploadedFileCommandHandler(BenchmarkingDbContext context, IMediator mediator, PathConfigs pathConfigs)
+    {
+        _context = context;
+        _mediator = mediator;
+        _workDir = pathConfigs.WorkingDir;
+        _setRoot = pathConfigs.SetFilesDir;
+    }
+    
+    public async Task<(Stream, string, string)> Handle(DownloadUploadedFileCommand request, CancellationToken cancellationToken)
+    {
+        var storePath = "";
+        var extension = Path.GetExtension(request.FileId);
+        switch (extension)
+        {
+            case ".zip":
+                var exe = await _context.Executables.Where(e => e.Path == request.FileId)
+                    .Select(e => new ExecutableHeader(e)).FirstOrDefaultAsync(cancellationToken);
+                storePath = Path.Join(_workDir, (exe ?? throw new ApplicationException($"File with id {request.FileId} was not found."))
+                    .UserName, "tools");
+                break;
+            case ".set":
+                var set = await _context.SetFiles.Where(e => e.Path == request.FileId)
+                    .Select(s => new SetFileHeader(s)).FirstOrDefaultAsync(cancellationToken);
+                if (set == null)
+                    throw new ApplicationException($"File with id {request.FileId} was not found.");
+                storePath = _setRoot;
+                break;
+        }
+        
+        var store = new CustomTusDiskStore(storePath, _mediator);
+        var file = await store.GetFileAsync(request.FileId, cancellationToken);
+
+        var fileStream = await file.GetContentAsync(cancellationToken);
+        var metadata = await file.GetMetadataAsync(cancellationToken);
+        
+        return (fileStream, TusUtil.GetContentTypeOrDefault(metadata, extension.TrimStart('.')), 
+            TusUtil.GetContentNameOrDefault(metadata, request.FileId));
+    }
+}
---
+++ BenchmarkingPortal.Bll/Features/UploadedFile/Commands/DownloadUploadedFileCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.UploadedFile.Commands;
+
+public class DownloadUploadedFileCommand : IRequest<(Stream, string, string)>
+{
+    public string FileId { get; init; } = null!;
+}
