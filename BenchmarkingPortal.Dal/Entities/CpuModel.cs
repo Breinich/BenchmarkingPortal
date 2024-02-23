5b70bd228340ae8a7033b59370799b668ba7f8e2
Breinich <vencel@bajnok.hu>
2024-02-23 21:45:07 +0100

    ✨ feat(CpuModel.cs): add new entity class CpuModel to represent CPU models in the benchmarking portal application

---
+++ BenchmarkingPortal.Dal/Entities/CpuModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkingPortal.Dal.Entities;
+
+public class CpuModel
+{
+    public int Id { get; set; }
+
+    public string Value { get; set; }
+
+    public string Name { get; set; }
+
+    public virtual ICollection<Worker> Workers { get; } = new List<Worker>();
+}
