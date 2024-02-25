1c0baab5fd3d99f79be0ee713b428c712e0ae21e
Breinich <vencel@bajnok.hu>
2024-02-25 17:40:57 +0100

    🔧 refactor(CpuModel.cs): simplify property initialization syntax and add missing Benchmarks collection 💡 chore(CpuModel.cs): improve code readability by using null-forgiving operator (!) for Value and Name properties

--- BenchmarkingPortal.Dal/Entities/CpuModel.cs
+++ BenchmarkingPortal.Dal/Entities/CpuModel.cs
@@ -9,10 +9,8 @@ namespace BenchmarkingPortal.Dal.Entities;
 public class CpuModel
 {
     public int Id { get; set; }
-
-    public string Value { get; set; }
-
-    public string Name { get; set; }
-
+    public string Value { get; set; } = null!;
+    public string Name { get; set; } = null!;
     public virtual ICollection<Worker> Workers { get; } = new List<Worker>();
+    public virtual ICollection<Benchmark> Benchmarks { get; } = new List<Benchmark>();
 }
