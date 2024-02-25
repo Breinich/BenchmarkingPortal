14864d4d62d98dee6cce29fa6f0e2bb34c5cbbea
Breinich <vencel@bajnok.hu>
2024-02-25 17:41:54 +0100

    🔧 chore(Executable.cs): remove unnecessary newlines and trailing whitespace for code cleanliness and consistency

--- BenchmarkingPortal.Dal/Entities/Executable.cs
+++ BenchmarkingPortal.Dal/Entities/Executable.cs
@@ -3,22 +3,13 @@
 public class Executable
 {
     public int Id { get; set; }
-
     public string OwnerTool { get; set; } = null!;
-
     public string ToolVersion { get; set; } = null!;
-
     public string Path { get; set; } = null!;
-
     public string Name { get; set; } = null!;
-
     public string Version { get; set; } = null!;
-
     public DateTime UploadedDate { get; set; }
-
     public string UserName { get; set; } = null!;
-
     public virtual ICollection<Benchmark> Benchmarks { get; } = new List<Benchmark>();
-
     public virtual User User { get; set; } = null!;
 }
