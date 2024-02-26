754dc9c67bbafa4a55ccccf9a3fce1aa47625d63
Breinich <vencel@bajnok.hu>
2024-02-26 15:29:46 +0100

    🔧 chore(BenchmarkingDbContext.cs): add missing entity configurations for CpuModel, PropertyFile, and SourceSet to enable database operations on these entities

--- BenchmarkingPortal.Dal/BenchmarkingDbContext.cs
+++ BenchmarkingPortal.Dal/BenchmarkingDbContext.cs
@@ -3,6 +3,7 @@ using BenchmarkingPortal.Dal.EntityConfigurations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace BenchmarkingPortal.Dal;
 
@@ -14,44 +15,33 @@ public class BenchmarkingDbContext : IdentityDbContext<User, IdentityRole<int>, 
     }
 
     public virtual DbSet<Benchmark> Benchmarks => Set<Benchmark>();
-
     public virtual DbSet<ComputerGroup> ComputerGroups => Set<ComputerGroup>();
-
     public virtual DbSet<Configuration> Configurations => Set<Configuration>();
-
     public virtual DbSet<ConfigurationItem> ConfigurationItems => Set<ConfigurationItem>();
-
     public virtual DbSet<Constraint> Constraints => Set<Constraint>();
-
+    public virtual DbSet<CpuModel> CpuModels => Set<CpuModel>();
     public virtual DbSet<Executable> Executables => Set<Executable>();
-
+    public virtual DbSet<PropertyFile> PropertyFiles => Set<PropertyFile>();
     public virtual DbSet<SetFile> SetFiles => Set<SetFile>();
-
+    public virtual DbSet<SourceSet> SourceSets => Set<SourceSet>();
     public virtual DbSet<Worker> Workers => Set<Worker>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-
-
+        
         builder.ApplyConfiguration(new BenchmarkEntityConfiguration());
-
         builder.ApplyConfiguration(new ComputerGroupEntityConfiguration());
-
         builder.ApplyConfiguration(new ConfigurationEntityConfiguration());
-
         builder.ApplyConfiguration(new ConfigurationItemEntityConfiguration());
-
         builder.ApplyConfiguration(new ConstraintEntityConfiguration());
-
+        builder.ApplyConfiguration(new CpuModelEntityConfiguration());
         builder.ApplyConfiguration(new ExecutableEntityConfiguration());
-
+        builder.ApplyConfiguration(new PropertyFileEntityConfiguration());
         builder.ApplyConfiguration(new SetFileEntityConfiguration());
-
+        builder.ApplyConfiguration(new SourceSetEntityConfiguration());
         builder.ApplyConfiguration(new WorkerEntityConfiguration());
-
         builder.ApplyConfiguration(new UserEntityConfiguration());
-
 
         SeedData(builder);
     }
