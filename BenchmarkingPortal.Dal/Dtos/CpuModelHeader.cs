78b00731f6cf830d8098279d127a6451497cb265
Breinich <vencel@bajnok.hu>
2024-02-26 15:52:41 +0100

    ✨ feat(CpuModelHeader.cs): add new class CpuModelHeader to represent a DTO for CpuModel entity
    The CpuModelHeader class is added to the BenchmarkingPortal.Dal.Dtos namespace. It is a Data Transfer Object (DTO) that represents a simplified version of the CpuModel entity. The class has properties for Id, Value, and Name, which are populated from the corresponding properties of the CpuModel entity. This DTO will be used for transferring CpuModel data between layers of the application.

---
+++ BenchmarkingPortal.Dal/Dtos/CpuModelHeader.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkingPortal.Dal.Entities;
+
+namespace BenchmarkingPortal.Dal.Dtos;
+
+public class CpuModelHeader
+{
+    public CpuModelHeader() { }
+
+    public CpuModelHeader(CpuModel cm)
+    {
+        Id = cm.Id;
+        Value = cm.Value;
+        Name = cm.Name;
+    }
+
+    public int Id { get; set; }
+    public string? Value { get; set; }
+    public string? Name { get; set; }
+}
