37e1a3e48e7ccfdc1f70b91a0cd400264bde8aff
Breinich <vencel@bajnok.hu>
2024-02-26 15:53:54 +0100

    🔧 refactor(ComputerGroup.cs): remove unused properties and add new properties for better representation of a computer group

--- BenchmarkingPortal.Dal/Entities/ComputerGroup.cs
+++ BenchmarkingPortal.Dal/Entities/ComputerGroup.cs
@@ -3,14 +3,9 @@
 public class ComputerGroup
 {
     public int Id { get; set; }
-
     public string? Description { get; set; }
-
-    public string Name { get; set; }
-
-    public string Url { get; set; }
-
+    public string Name { get; set; } = null!;
+    public string Hostname { get; set; } = null!;
     public virtual ICollection<Benchmark> Benchmarks { get; } = new List<Benchmark>();
-
     public virtual ICollection<Worker> Workers { get; } = new List<Worker>();
 }
