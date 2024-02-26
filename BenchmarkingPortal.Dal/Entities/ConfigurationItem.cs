9893498deaa953d3be10c8119fb35b2450e0f54b
Breinich <vencel@bajnok.hu>
2024-02-26 15:54:44 +0100

    🔧 chore(ConfigurationItem.cs): remove unnecessary newlines and trailing whitespace for code cleanliness

--- BenchmarkingPortal.Dal/Entities/ConfigurationItem.cs
+++ BenchmarkingPortal.Dal/Entities/ConfigurationItem.cs
@@ -3,10 +3,7 @@
 public class ConfigurationItem
 {
     public string Key { get; set; } = null!;
-
     public string Value { get; set; } = null!;
-
     public Scope Scope { get; set; } = Scope.Local;
-
     public int ConfigurationId { get; set; }
 }
