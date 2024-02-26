380711a597589f10b90df3ecdde6ce70436f4175
Breinich <vencel@bajnok.hu>
2024-02-26 17:35:18 +0100

    📝 docs(ExceptionMessage.cs): add summary and type parameter documentation to ExceptionMessage class

--- BenchmarkingPortal.Bll/Exceptions/ExceptionMessage.cs
+++ BenchmarkingPortal.Bll/Exceptions/ExceptionMessage.cs
@@ -1,5 +1,9 @@
 ﻿namespace BenchmarkingPortal.Bll.Exceptions;
 
+/// <summary>
+/// Generic exception messages for the given type.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class ExceptionMessage<T>
 {
     public string ObjectNotFound { get; } = "The " + typeof(T).Name + " with the given Id is not found.";
