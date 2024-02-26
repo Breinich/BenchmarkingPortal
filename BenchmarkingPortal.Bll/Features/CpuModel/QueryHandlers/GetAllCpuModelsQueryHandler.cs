0245e78953d9baea13508856ebe110021c883b69
Breinich <vencel@bajnok.hu>
2024-02-26 15:27:38 +0100

    📦 chore(GetAllCpuModelsQueryHandler.cs): add GetAllCpuModelsQueryHandler class to handle GetAllCpuModelsQuery 🐛 fix(GetAllCpuModelsQueryHandler.cs): add missing newline at the end of the file for consistency

---
+++ BenchmarkingPortal.Bll/Features/CpuModel/QueryHandlers/GetAllCpuModelsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkingPortal.Bll.Features.CpuModel.Queries;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.CpuModel.QueryHandlers;
+
+public class GetAllCpuModelsQueryHandler : IRequestHandler<GetAllCpuModelsQuery, IEnumerable<CpuModelHeader>>
+{
+    private readonly BenchmarkingDbContext _context;
+    
+    public GetAllCpuModelsQueryHandler(BenchmarkingDbContext context)
+    {
+        _context = context;
+    }
+    
+    public Task<IEnumerable<CpuModelHeader>> Handle(GetAllCpuModelsQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException(
+            "Needs a migration to let the CpuModels DBSet be available in the DB context."
+            );
+    }
+}
