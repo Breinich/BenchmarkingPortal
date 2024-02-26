d5a74cb63e545cf0cba017799d7dda86429774c0
Breinich <vencel@bajnok.hu>
2024-02-26 17:48:47 +0100

    🔥 refactor(GetResultPathQuery.cs): remove unused GetResultPathQuery and GetResultPathQueryHandler files
    The GetResultPathQuery.cs and GetResultPathQueryHandler.cs files were no longer being used and were deleted to improve code cleanliness and remove unnecessary code.

--- BenchmarkingPortal.Bll/Features/Benchmark/Queries/GetResultPathQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace BenchmarkingPortal.Bll.Features.Benchmark.Queries;
-
-public class GetResultPathQuery : IRequest<string>
-{
-    public int BenchmarkId { get; init; }
-}
