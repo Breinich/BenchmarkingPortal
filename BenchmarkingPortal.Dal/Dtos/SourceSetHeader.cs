6014338439efeb8911f6e75bfda6dcae8ef1da71
Breinich <vencel@bajnok.hu>
2024-02-26 15:53:30 +0100

    🎉 feat(SourceSetHeader.cs): add SourceSetHeader class to represent a header for a source set
    The SourceSetHeader class is added to the BenchmarkingPortal.Dal.Dtos namespace. It is used to represent a header for a source set. The class has properties for Id, Name, Root, and UserName. It also includes a constructor that takes a SourceSet object and initializes the properties based on its values.

---
+++ BenchmarkingPortal.Dal/Dtos/SourceSetHeader.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkingPortal.Dal.Entities;
+
+namespace BenchmarkingPortal.Dal.Dtos;
+
+public class SourceSetHeader
+{
+    public SourceSetHeader() { }
+
+    public SourceSetHeader(SourceSet sc)
+    {
+        Id = sc.Id;
+        Name = sc.Name;
+        Root = sc.Root;
+        UserName = sc.UserName;
+    }
+
+    public int Id { get; init; }
+    public string? Name { get; init; }
+    public string? Root { get; init; }
+    public string? UserName { get; init; }
+}
