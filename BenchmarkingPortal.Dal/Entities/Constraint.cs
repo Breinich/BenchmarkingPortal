5883d7c02246e82b070e04901c2ade7534ee770e
Breinich <vencel@bajnok.hu>
2024-02-26 15:54:57 +0100

    🔧 chore(Constraint.cs): remove unnecessary newlines at the end of the file to improve code cleanliness and consistency

--- BenchmarkingPortal.Dal/Entities/Constraint.cs
+++ BenchmarkingPortal.Dal/Entities/Constraint.cs
@@ -3,8 +3,6 @@
 public class Constraint
 {
     public string Premise { get; set; } = null!;
-
     public string Consequence { get; set; } = null!;
-
     public int ConfigurationId { get; set; }
 }
