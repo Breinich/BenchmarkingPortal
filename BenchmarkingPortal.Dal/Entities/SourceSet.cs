5dda03ac6966c97c585e7cd9bf10b315c620b7a8
Breinich <vencel@bajnok.hu>
2024-02-26 15:56:16 +0100

    📦 chore(SourceSet.cs): add SourceSet entity class
    📦 chore(SourceSetEntityConfiguration.cs): add SourceSet entity configuration class
    
    The SourceSet.cs file adds a new entity class called SourceSet. This class represents a set of source files in the benchmarking portal. It has properties such as Id, Name, Root, UserName, and collections of SetFiles and PropertyFiles. It also has a navigation property User to represent the user who owns the source set.
    
    The SourceSetEntityConfiguration.cs file adds the entity configuration class for the SourceSet entity. It configures the table name, primary key, and properties of the SourceSet entity. It also configures the foreign key relationship between SourceSet and User entities.

---
+++ BenchmarkingPortal.Dal/Entities/SourceSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkingPortal.Dal.Entities;
+public class SourceSet
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Root { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+    public virtual ICollection<SetFile> SetFiles { get; } = new List<SetFile>();
+    public virtual ICollection<PropertyFile> PropertyFiles { get; } = new List<PropertyFile>();
+    public virtual User User { get; set; } = null!;
+}
+
---
+++ BenchmarkingPortal.Dal/EntityConfigurations/SourceSetEntityConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkingPortal.Dal.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkingPortal.Dal.EntityConfigurations;
+
+public class SourceSetEntityConfiguration : IEntityTypeConfiguration<SourceSet>
+{
+    public void Configure(EntityTypeBuilder<SourceSet> builder)
+    {
+        builder.ToTable("SourceSets");
+
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.Name).HasMaxLength(50).IsRequired();
+        builder.Property(s => s.Root).HasMaxLength(50).IsRequired();
+
+        builder.HasOne(s => s.User).WithMany(u => u.SourceSets)
+            .HasForeignKey(s => s.UserName)
+            .HasPrincipalKey(u => u.UserName)
+            .OnDelete(DeleteBehavior.SetNull)
+            .HasConstraintName("FK_SourceSet_User");
+
+        SampleData(builder);
+    }
+
+    private void SampleData(EntityTypeBuilder<SourceSet> builder)
+    {
+
+    }
+}
