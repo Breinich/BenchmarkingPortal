8b518356c2d67476e1e25a396ab829ebc5fd2a24
Breinich <vencel@bajnok.hu>
2024-02-26 15:55:31 +0100

    ✨ feat(PropertyFile.cs): add new entity class PropertyFile to represent a property file in the benchmarking portal application

---
+++ BenchmarkingPortal.Dal/Entities/PropertyFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkingPortal.Dal.Entities;
+
+public class PropertyFile
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Path { get; set; } = null!;
+    public string Version { get; set; } = null!;
+    public int SourceSetId { get; set; }
+    public DateTime UploadedDate { get; set; }
+    public string UserName { get; set; } = null!;
+    public virtual User User { get; set; } = null!;
+    public virtual SourceSet SourceSet { get; set; } = null!;
+}
