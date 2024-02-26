5b65dc4f4bf8d0a1f8704847673dd4445af03958
Breinich <vencel@bajnok.hu>
2024-02-26 15:57:17 +0100

    🔧 fix(User.cs): make Benchmarks, Executables, SetFiles, PropertyFiles, and SourceSets read-only to enforce immutability and prevent accidental modification

--- BenchmarkingPortal.Dal/Entities/User.cs
+++ BenchmarkingPortal.Dal/Entities/User.cs
@@ -6,7 +6,9 @@ public class User : IdentityUser<int>
 {
     public bool Subscription { get; set; } = true;
 
-    public virtual ICollection<Benchmark> Benchmarks { get; set; } = new List<Benchmark>();
-    public virtual ICollection<Executable> Executables { get; set; } = new List<Executable>();
-    public virtual ICollection<SetFile> SetFiles { get; set; } = new List<SetFile>();
+    public virtual ICollection<Benchmark> Benchmarks { get; } = new List<Benchmark>();
+    public virtual ICollection<Executable> Executables { get; } = new List<Executable>();
+    public virtual ICollection<SetFile> SetFiles { get; } = new List<SetFile>();
+    public virtual ICollection<PropertyFile> PropertyFiles { get; } = new List<PropertyFile>();
+    public virtual ICollection<SourceSet> SourceSets { get; } = new List<SourceSet>();
 }
