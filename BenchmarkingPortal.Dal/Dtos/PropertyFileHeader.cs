b089bde8ede32d34ba7d66a94bda190953bcab7f
Breinich <vencel@bajnok.hu>
2024-02-26 15:53:11 +0100

    📦 chore(PropertyFileHeader.cs): add PropertyFileHeader class to store information about a property file 🔒 chore(PropertyFileHeader.cs): add private setters to properties of PropertyFileHeader class for immutability and encapsulation

---
+++ BenchmarkingPortal.Dal/Dtos/PropertyFileHeader.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkingPortal.Dal.Entities;
+
+namespace BenchmarkingPortal.Dal.Dtos;
+
+public class PropertyFileHeader
+{
+    public PropertyFileHeader() { }
+
+    public PropertyFileHeader(PropertyFile pf)
+    {
+        Id = pf.Id;
+        Name = pf.Name;
+        Path = pf.Path;
+        Version = pf.Version;
+        SourceSetId = pf.SourceSetId;
+        UploadedDate = pf.UploadedDate;
+        UserName = pf.UserName;
+    }
+
+    public int Id { get; init; }
+    public string? Name { get; init; }
+    public string? Path { get; init; }
+    public string? Version { get; init; }
+    public int SourceSetId { get; init; }
+    public DateTime UploadedDate { get; init; }
+    public string? UserName { get; init; }
+}
