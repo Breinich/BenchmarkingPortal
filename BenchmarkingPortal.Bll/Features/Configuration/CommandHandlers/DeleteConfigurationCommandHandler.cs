c15e18624d57fbc24c47588ba3ec9769883c6b86
Breinich <vencel@bajnok.hu>
2024-03-11 17:01:09 +0100

    docs(DeleteConfigurationCommand.cs, DeleteConfigurationCommandHandler.cs) add summary comments

--- BenchmarkingPortal.Bll/Features/Configuration/CommandHandlers/DeleteConfigurationCommandHandler.cs
+++ BenchmarkingPortal.Bll/Features/Configuration/CommandHandlers/DeleteConfigurationCommandHandler.cs
@@ -1,10 +1,15 @@
-﻿using BenchmarkingPortal.Bll.Features.Configuration.Commands;
+﻿using BenchmarkingPortal.Bll.Exceptions;
+using BenchmarkingPortal.Bll.Features.Configuration.Commands;
 using BenchmarkingPortal.Dal;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace BenchmarkingPortal.Bll.Features.Configuration.CommandHandlers;
 
+/// <summary>
+/// The handler for the <see cref="DeleteConfigurationCommand"/>.
+/// </summary>
+// ReSharper disable once UnusedType.Global
 public class DeleteConfigurationCommandHandler : IRequestHandler<DeleteConfigurationCommand>
 {
     private readonly BenchmarkingDbContext _context;
@@ -18,8 +23,11 @@ public class DeleteConfigurationCommandHandler : IRequestHandler<DeleteConfigura
     {
         var configuration = await _context.Configurations
             .Where(c => c.Id == request.Id).Include(c => c.ConfigurationItems)
-            .Include(c => c.Constraints).FirstAsync(cancellationToken);
+            .Include(c => c.Constraints).FirstOrDefaultAsync(cancellationToken);
 
+        if (configuration is null)
+            throw new ArgumentException(new ExceptionMessage<Dal.Entities.Configuration>().ObjectNotFound);
+        
         foreach (var item in configuration.ConfigurationItems)
         {
             _context.Remove(item);
@@ -30,10 +38,7 @@ public class DeleteConfigurationCommandHandler : IRequestHandler<DeleteConfigura
             _context.Remove(constraint);
         }
         
-        if (configuration is null)
-            throw new ArgumentException("The configuration with the given id doesn't exist!");
         _context.Configurations.Remove(configuration);
-        
         await _context.SaveChangesAsync(cancellationToken);
     }
 }
--- BenchmarkingPortal.Bll/Features/Configuration/Commands/DeleteConfigurationCommand.cs
+++ BenchmarkingPortal.Bll/Features/Configuration/Commands/DeleteConfigurationCommand.cs
@@ -2,6 +2,9 @@
 
 namespace BenchmarkingPortal.Bll.Features.Configuration.Commands;
 
+/// <summary>
+/// Command for deleting a configuration
+/// </summary>
 public class DeleteConfigurationCommand : IRequest
 {
     public int Id { get; init; }
