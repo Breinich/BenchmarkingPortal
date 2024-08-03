ad22d5770c3100ee79e90897895c27daff4afdc8
Breinich <vencel@bajnok.hu>
2024-08-03 15:32:43 +0200

    add GetAllPropertyFileNamesBySourceSetQuery

---
+++ BenchmarkingPortal.Bll/Features/PropertyFile/Queries/GetAllPropertyFilesQuery.cs
@@ -0,0 +1,7 @@
+﻿using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.PropertyFile.Queries
+{
+    public class GetAllPropertyFilesQuery : IRequest<IEnumerable<PropertyFileHeader>> {}
+}
---
+++ BenchmarkingPortal.Bll/Features/PropertyFile/QueryHandlers/GetAllPropertyFilesQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkingPortal.Bll.Features.PropertyFile.Queries;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkingPortal.Bll.Features.PropertyFile.QueryHandlers;
+
+public class GetAllPropertyFilesQueryHandler(BenchmarkingDbContext context) : IRequestHandler<GetAllPropertyFilesQuery, IEnumerable<PropertyFileHeader>>
+{
+    public async Task<IEnumerable<PropertyFileHeader>> Handle(GetAllPropertyFilesQuery request, CancellationToken cancellationToken)
+    {
+        return await context.PropertyFiles.Select(p => new  PropertyFileHeader(p)).ToListAsync(cancellationToken);
+    }
+}
