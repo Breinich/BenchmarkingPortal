d3dc10be409add788b0a5a367c17dd2a2f4c2418
Breinich <vencel@bajnok.hu>
2024-08-03 15:37:43 +0200

    add GetAllSourceSetsQuery

---
+++ BenchmarkingPortal.Bll/Features/SourceSet/Queries/GetAllSourceSetsQuery.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.Queries;
+
+public class GetAllSourceSetsQuery : IRequest<IEnumerable<SourceSetHeader>> {}
---
+++ BenchmarkingPortal.Bll/Features/SourceSet/QueryHandlers/GetAllSourceSetsQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkingPortal.Bll.Features.SourceSet.Queries;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.QueryHandlers;
+
+public class GetAllSourceSetsQueryHandler(BenchmarkingDbContext context) 
+    : IRequestHandler<GetAllSourceSetsQuery, IEnumerable<SourceSetHeader>>
+{
+    public async Task<IEnumerable<SourceSetHeader>> Handle(GetAllSourceSetsQuery request, CancellationToken cancellationToken)
+    {
+        return await context.SourceSets.Select(s => new SourceSetHeader(s)).ToListAsync(cancellationToken);
+    }
+}
