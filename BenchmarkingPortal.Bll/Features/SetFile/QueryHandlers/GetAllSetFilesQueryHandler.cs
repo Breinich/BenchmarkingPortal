d75c8d5869e183a995a16fccbfd8cebd12ef9d6c
Breinich <vencel@bajnok.hu>
2024-08-03 16:47:33 +0200

    fix GetAllSetFilesQueryHandler

--- BenchmarkingPortal.Bll/Features/SetFile/QueryHandlers/GetAllSetFilesQueryHandler.cs
+++ BenchmarkingPortal.Bll/Features/SetFile/QueryHandlers/GetAllSetFilesQueryHandler.cs
@@ -18,9 +18,7 @@ public class GetAllSetFilesQueryHandler : IRequestHandler<GetAllSetFilesQuery, I
 
     public async Task<IEnumerable<SetFileHeader>> Handle(GetAllSetFilesQuery request,
         CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException("This feature is not fully implemented yet.");
-        
+    {   
         return await _context.SetFiles.Select(s => new SetFileHeader(s)).ToListAsync(cancellationToken);
     }
 }
