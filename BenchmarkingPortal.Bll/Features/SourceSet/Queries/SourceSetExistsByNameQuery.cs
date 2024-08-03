084de45946cba6f4d5fcac68b1f9d904b62a9a48
Breinich <vencel@bajnok.hu>
2024-08-03 15:46:51 +0200

    add SourceSetExistsByNameQuery

---
+++ BenchmarkingPortal.Bll/Features/SourceSet/Queries/SourceSetExistsByNameQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.Queries;
+
+public class SourceSetExistsByNameQuery : IRequest<bool>
+{
+    public string FileName { get; init; } = null!;
+}
---
+++ BenchmarkingPortal.Bll/Features/SourceSet/QueryHandlers/SourceSetExistsByNameQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkingPortal.Bll.Features.SourceSet.Queries;
+using BenchmarkingPortal.Dal;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.QueryHandlers;
+
+public class SourceSetExistsByNameQueryHandler(BenchmarkingDbContext context) : IRequestHandler<SourceSetExistsByNameQuery, bool>
+{
+    public async Task<bool> Handle(SourceSetExistsByNameQuery request, CancellationToken cancellationToken)
+    {
+        return await context.SourceSets.AnyAsync(x => x.Path == request.FileName, cancellationToken);
+    }
+}
