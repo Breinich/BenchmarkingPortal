170da33c6a2edfd39909926daab754740a76b7d7
Breinich <vencel@bajnok.hu>
2024-08-03 15:38:25 +0200

    add GetSetFileNamesBySourceSetIdQuery

---
+++ BenchmarkingPortal.Bll/Features/SetFile/Queries/GetSetFileNamesBySourceSetIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.SetFile.Queries;
+
+public class GetSetFileNamesBySourceSetIdQuery : IRequest<IEnumerable<string>>
+{
+    public int SourceSetId { get; init; }
+}
---
+++ BenchmarkingPortal.Bll/Features/SetFile/QueryHandlers/GetSetFileNamesBySourceSetIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkingPortal.Bll.Exceptions;
+using BenchmarkingPortal.Bll.Features.SetFile.Queries;
+using BenchmarkingPortal.Bll.Services;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkingPortal.Bll.Features.SetFile.QueryHandlers;
+
+public class GetSetFileNamesBySourceSetIdQueryHandler(BenchmarkingDbContext context, PathConfigs pathConfigs) : IRequestHandler<GetSetFileNamesBySourceSetIdQuery, IEnumerable<string>>
+{
+    public async Task<IEnumerable<string>> Handle(GetSetFileNamesBySourceSetIdQuery request, CancellationToken cancellationToken)
+    {
+        var sourceSet = await context.SourceSets.Where(s => s.Id == request.SourceSetId)
+                            .Select(s => new SourceSetHeader(s)).FirstOrDefaultAsync(cancellationToken) 
+                        ?? throw new ArgumentException(ExceptionMessage<Dal.Entities.SourceSet>.ObjectNotFound);
+
+        if (string.IsNullOrWhiteSpace(sourceSet.SetFilesDir))
+            throw new ArgumentException(ExceptionMessage<Dal.Entities.SourceSet>.ObjectNotFound);
+        
+        return [.. Directory.GetFiles(
+            Path.Join(
+                pathConfigs.WorkingDir, 
+                sourceSet.UserName, 
+                pathConfigs.SourceSetDir, 
+                sourceSet.SetFilesDir
+                ), "*.set", SearchOption.TopDirectoryOnly)
+        ];
+    }
+}
