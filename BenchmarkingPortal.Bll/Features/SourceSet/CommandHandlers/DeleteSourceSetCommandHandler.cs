c4c33f72b305c4f730fef48a3a3fd4c43b0a4228
Breinich <vencel@bajnok.hu>
2024-08-03 15:38:59 +0200

    add DeleteSourceSetCommand

---
+++ BenchmarkingPortal.Bll/Features/SourceSet/CommandHandlers/DeleteSourceSetCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkingPortal.Bll.Exceptions;
+using BenchmarkingPortal.Bll.Features.SourceSet.Commands;
+using BenchmarkingPortal.Bll.Services;
+using BenchmarkingPortal.Bll.Tus;
+using BenchmarkingPortal.Dal;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.CommandHandlers;
+
+public class DeleteSourceSetCommandHandler(BenchmarkingDbContext context, UserManager<Dal.Entities.User> userManager, 
+    PathConfigs pathConfigs) : IRequestHandler<DeleteSourceSetCommand>
+{
+    public async Task Handle(DeleteSourceSetCommand request, CancellationToken cancellationToken)
+    {
+        var sourceSet = await context.SourceSets.FindAsync([request.Id], cancellationToken: cancellationToken) ??
+                       throw new ArgumentException(ExceptionMessage<Dal.Entities.SourceSet>.ObjectNotFound);
+
+        if (sourceSet.UserName != request.InvokerName)
+        {
+            var user = await userManager.FindByNameAsync(request.InvokerName) ??
+                       throw new ArgumentException(ExceptionMessage<Dal.Entities.User>.ObjectNotFound);
+
+            var admin = await userManager.IsInRoleAsync(user, Roles.Admin);
+
+            if (!admin) throw new ArgumentException(ExceptionMessage<Dal.Entities.SourceSet>.NoPrivilege);
+        }
+
+        var setFilesStore = new CustomTusDiskStore(Path.Join(
+            pathConfigs.WorkingDir, sourceSet.UserName, pathConfigs.SourceSetDir, 
+            sourceSet.Name, pathConfigs.SetFileDir));
+        var setFiles = await context.SetFiles.Where(f => f.SourceSetId == request.Id).ToListAsync(cancellationToken);
+        foreach (var setFile in setFiles)
+        {
+            context.Remove(setFile);
+            await context.SaveChangesAsync(cancellationToken);
+            
+            await setFilesStore.DeleteFileAsync(setFile.Path, cancellationToken);
+        }
+        
+        context.Remove(sourceSet);
+        await context.SaveChangesAsync(cancellationToken);
+        
+        var sourceSetStore = new CustomTusDiskStore(Path.Join(pathConfigs.WorkingDir, sourceSet.UserName, pathConfigs.SourceSetDir));
+        await sourceSetStore.DeleteFileAsync(sourceSet.Path, cancellationToken);
+        
+        new Task(() => Directory.Delete(Path.Join(pathConfigs.WorkingDir, sourceSet.UserName, pathConfigs.SourceSetDir, sourceSet.Name), true)).Start();
+    }
+}
---
+++ BenchmarkingPortal.Bll/Features/SourceSet/Commands/DeleteSourceSetCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.Commands;
+
+public class DeleteSourceSetCommand : IRequest
+{
+    public int Id { get; init; }
+    public string FileId { get; init; } = null!;
+    public string InvokerName { get; init; } = null!;
+}
