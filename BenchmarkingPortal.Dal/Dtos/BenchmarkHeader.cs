6f06c50f7a79c734969dd2f0044015add2eb4574
Breinich <vencel@bajnok.hu>
2024-08-03 15:27:32 +0200

    improve documentation

--- BenchmarkingPortal.Dal/Dtos/BenchmarkHeader.cs
+++ BenchmarkingPortal.Dal/Dtos/BenchmarkHeader.cs
@@ -2,12 +2,20 @@
 
 namespace BenchmarkingPortal.Dal.Dtos;
 
+/// <summary>
+/// Represents a <see cref="Benchmark"/>'s header.
+/// </summary>
 public class BenchmarkHeader
 {
-    public BenchmarkHeader()
-    {
-    }
+    /// <summary>
+    /// Default constructor for BenchmarkHeader.
+    /// </summary>
+    public BenchmarkHeader() { }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BenchmarkHeader"/> class.
+    /// </summary>
+    /// <param name="b">The <see cref="Benchmark"/> entity.</param>
     public BenchmarkHeader(Benchmark b)
     {
         Id = b.Id;
@@ -30,24 +38,98 @@ public class BenchmarkHeader
         VcloudId = b.VcloudId;
     }
 
+    /// <summary>
+    /// <see cref="Benchmark.Id"/>
+    /// </summary>
     public int Id { get; init; }
+
+    /// <summary>
+    /// <see cref="Benchmark.Name"/>
+    /// </summary>
     public string? Name { get; init; }
+
+    /// <summary>
+    /// <see cref="Benchmark.Priority"/>
+    /// </summary>
     public Priority Priority { get; set; }
+
+    /// <summary>
+    /// <see cref="Benchmark.Status"/>
+    /// </summary>
     public Status Status { get; set; }
+
+    /// <summary>
+    /// <see cref="Benchmark.Ram"/>
+    /// </summary>
     public int Ram { get; init; }
+
+    /// <summary>
+    /// <see cref="Benchmark.Cpu"/>
+    /// </summary>
     public int Cpu { get; init; }
+
+    /// <summary>
+    /// <see cref="Benchmark.ResultPath"/>
+    /// </summary>
     public string? ResultPath { get; set; }
+
+    /// <summary>
+    /// <see cref="Benchmark.TimeLimit"/>
+    /// </summary>
     public int TimeLimit { get; init; }
+
+    /// <summary>
+    /// <see cref="Benchmark.HardTimeLimit"/>
+    /// </summary>
     public int HardTimeLimit { get; init; }
+
+    /// <summary>
+    /// <see cref="Benchmark.CpuModelId"/>
+    /// </summary>
     public int CpuModelId { get; init; }
+
+    /// <summary>
+    /// <see cref="Benchmark.ComputerGroupId"/>
+    /// </summary>
     public int ComputerGroupId { get; init; }
+
+    /// <summary>
+    /// <see cref="Benchmark.ExecutableId"/>
+    /// </summary>
     public int ExecutableId { get; init; }
+
+    /// <summary>
+    /// <see cref="Benchmark.SetFilePath"/>
+    /// </summary>
     public string? SetFilePath { get; init; }
+
+    /// <summary>
+    /// <see cref="Benchmark.PropertyFilePath"/>
+    /// </summary>
     public string? PropertyFilePath { get; init; }
+
+    /// <summary>
+    /// <see cref="Benchmark.StartedDate"/>
+    /// </summary>
     public DateTime StartedDate { get; set; }
+
+    /// <summary>
+    /// <see cref="Benchmark.ConfigurationId"/>
+    /// </summary>
     public int ConfigurationId { get; init; }
+
+    /// <summary>
+    /// <see cref="Benchmark.UserName"/>
+    /// </summary>
     public string UserName { get; init; } = null!;
-    
+
+    /// <summary>
+    /// <see cref="Benchmark.VcloudId"/>
+    /// </summary>
     public string? VcloudId { get; set; }
+
+    /// <summary>
+    /// The name of the target CPU model.
+    /// </summary>
     public string? CpuModelValue { get; init; }
 }
--- BenchmarkingPortal.Dal/Entities/Benchmark.cs
+++ BenchmarkingPortal.Dal/Entities/Benchmark.cs
@@ -1,28 +1,122 @@
 ﻿namespace BenchmarkingPortal.Dal.Entities;
 
+/// <summary>
+/// Represents a benchmark entity.
+/// </summary>
 public class Benchmark
 {
+    /// <summary>
+    /// The id of the benchmark.
+    /// </summary>
     public int Id { get; set; }
+    
+    /// <summary>
+    /// The name of the benchmark.
+    /// </summary>
     public string Name { get; set; } = null!;
+    
+    /// <summary>
+    /// The priority of the benchmark.
+    /// </summary>
     public Priority Priority { get; set; }
+    
+    /// <summary>
+    /// The status of the benchmark.
+    /// </summary>
     public Status Status { get; set; }
+    
+    /// <summary>
+    /// The amount of RAM the benchmark can use.
+    /// </summary>
     public int Ram { get; set; }
+    
+    /// <summary>
+    /// The amount of CPU cores the benchmark can use.
+    /// </summary>
     public int Cpu { get; set; }
+    
+    /// <summary>
+    /// The path where the results are stored, relative to the working directory.
+    /// </summary>
     public string? ResultPath { get; set; }
+    
+    /// <summary>
+    /// The time limit of the benchmark.
+    /// </summary>
     public int TimeLimit { get; set; }
+    
+    /// <summary>
+    /// The hard time limit of the benchmark.
+    /// </summary>
     public int HardTimeLimit { get; set; }
+    
+    /// <summary>
+    /// The id of the attached computer group.
+    /// </summary>
     public int ComputerGroupId { get; set; }
+    
+    /// <summary>
+    /// The id of the used executable.
+    /// </summary>
     public int ExecutableId { get; set; }
+    
+    /// <summary>
+    /// The id of the used CPU model.
+    /// </summary>
     public int CpuModelId { get; set; }
+    
+    /// <summary>
+    /// The path to the set file, relative to the working directory.
+    /// </summary>
     public string SetFilePath { get; set; } = null!;
+    
+    /// <summary>
+    /// The path to the property file, relative to the working directory.
+    /// </summary>
     public string PropertyFilePath { get; set; } = null!;
+    
+    /// <summary>
+    /// The date the benchmark was started.
+    /// </summary>
     public DateTime StartedDate { get; set; }
+    
+    /// <summary>
+    /// The id of the used configuration.
+    /// </summary>
     public int ConfigurationId { get; set; }
+    
+    /// <summary>
+    /// The username of the user who started the benchmark.
+    /// </summary>
     public string UserName { get; set; } = null!;
+    
+    /// <summary>
+    /// The id of the benchmark in the Verifier Cloud.
+    /// </summary>
     public string? VcloudId { get; set; }
+    
+    /// <summary>
+    /// The used computer group.
+    /// </summary>
     public virtual ComputerGroup? ComputerGroup { get; set; }
+    
+    /// <summary>
+    /// The used configuration.
+    /// </summary>
     public virtual Configuration Configuration { get; set; } = null!;
+    
+    /// <summary>
+    /// The used executable.
+    /// </summary>
     public virtual Executable Executable { get; set; } = null!;
+    
+    /// <summary>
+    /// The owner of the benchmark.
+    /// </summary>
     public virtual User User { get; set; } = null!;
+    
+    /// <summary>
+    /// The used CPU model.
+    /// </summary>
     public virtual CpuModel? CpuModel { get; set; }
 }
