4ff52002c3b9eff9e815403d0b09bb0f869f1fa5
Breinich <vencel@bajnok.hu>
2024-08-03 15:45:25 +0200

    add UploadNewSourceSetCommand

---
+++ BenchmarkingPortal.Bll/Features/SourceSet/CommandHandlers/UploadNewSourceSetCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkingPortal.Bll.Features.SourceSet.Commands;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.CommandHandlers;
+
+public class UploadNewSourceSetCommandHandler(BenchmarkingDbContext context)
+    : IRequestHandler<UploadNewSourceSetCommand, SourceSetHeader>
+{
+    public async Task<SourceSetHeader> Handle(UploadNewSourceSetCommand request, CancellationToken cancellationToken)
+    {
+        var sourceSet = new Dal.Entities.SourceSet
+        {
+            Name = request.Name,
+            UserName = request.InvokerName,
+            UploadedDate = request.UploadedDate,
+            Path = request.Path
+        };
+        
+        await context.SourceSets.AddAsync(sourceSet, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+        
+        return new SourceSetHeader(sourceSet);
+    }
+}
---
+++ BenchmarkingPortal.Bll/Features/SourceSet/Commands/UploadNewSourceSetCommand.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.Commands;
+
+public class UploadNewSourceSetCommand : IRequest<SourceSetHeader>
+{
+    public string Name { get; init; } = null!;
+    public string Path { get; init; } = null!;
+    public DateTime UploadedDate { get; init; }
+    public string InvokerName { get; init; } = null!;
+}
