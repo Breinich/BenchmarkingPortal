f2adbe2793fd91be8b3b7a5c7a4e87daae6e1cb6
Breinich <vencel@bajnok.hu>
2024-08-03 15:38:41 +0200

    add GetSourceSetByIdQuery

---
+++ BenchmarkingPortal.Bll/Features/SourceSet/Queries/GetSourceSetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.Queries;
+
+public class GetSourceSetByIdQuery : IRequest<SourceSetHeader?>
+{
+    public int Id { get; init; }
+}
---
+++ BenchmarkingPortal.Bll/Features/SourceSet/QueryHandlers/GetSourceSetByIdQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkingPortal.Bll.Features.SourceSet.Queries;
+using BenchmarkingPortal.Dal;
+using BenchmarkingPortal.Dal.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkingPortal.Bll.Features.SourceSet.QueryHandlers;
+
+public class GetSourceSetByIdQueryHandler(BenchmarkingDbContext context) : IRequestHandler<GetSourceSetByIdQuery, SourceSetHeader?>
+{
+    public async Task<SourceSetHeader?> Handle(GetSourceSetByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await context.SourceSets.Where(s => s.Id == request.Id).Select(s => new SourceSetHeader(s)).FirstOrDefaultAsync(cancellationToken);
+    }
+}
