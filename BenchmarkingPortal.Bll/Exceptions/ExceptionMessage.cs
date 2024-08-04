fa8609b6672ed0b77eb9db21d4c42577830cb658
Breinich <vencel@bajnok.hu>
2024-08-04 16:05:21 +0200

    🐛 Fix: Added exception message for when a resource is in use and cannot be deleted.

--- BenchmarkingPortal.Bll/Exceptions/ExceptionMessage.cs
+++ BenchmarkingPortal.Bll/Exceptions/ExceptionMessage.cs
@@ -8,4 +8,5 @@ public static class ExceptionMessage<T>
 {
     public static string ObjectNotFound { get; } = "The " + typeof(T).Name + " with the given Id is not found.";
     public static string NoPrivilege { get; } = "The user is neither the owner of the " + typeof(T).Name + ", nor an admin.";
+    public static string InUse { get; } = "The " + typeof(T).Name + " is in use and cannot be deleted.";
 }
