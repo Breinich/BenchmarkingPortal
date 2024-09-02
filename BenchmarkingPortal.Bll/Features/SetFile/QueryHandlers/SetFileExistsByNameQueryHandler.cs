949fd2f0f636ac6bc8fd2369a66b2183ea071484
Breinich <vencel@bajnok.hu>
2024-09-02 19:04:42 +0200

    🐛 Refactor code: Update SetFileExistsByNameQueryHandler with a summary and Handle method for better readability and maintainability.

--- BenchmarkingPortal.Bll/Features/SetFile/QueryHandlers/SetFileExistsByNameQueryHandler.cs
+++ BenchmarkingPortal.Bll/Features/SetFile/QueryHandlers/SetFileExistsByNameQueryHandler.cs
@@ -7,9 +7,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BenchmarkingPortal.Bll.Features.SetFile.QueryHandlers;
 
+/// <summary>
+/// Query handler for <see cref="SetFileExistsByNameQuery"/>.
+/// </summary>
+/// <param name="dbContext"> <see cref="BenchmarkingDbContext"/> instance. </param>
+/// <param name="pathConfigs"> <see cref="PathConfigs"/> instance. </param>
 public class SetFileExistsByNameQueryHandler(BenchmarkingDbContext dbContext, PathConfigs pathConfigs)
     : IRequestHandler<SetFileExistsByNameQuery, bool>
 {
+    
+    /// <summary>
+    /// Handle the <see cref="SetFileExistsByNameQuery"/>.
+    /// </summary>
+    /// <param name="request"> <see cref="SetFileExistsByNameQuery"/> instance. </param>
+    /// <param name="cancellationToken"> <see cref="CancellationToken"/> instance. </param>
+    /// <returns> <see cref="Task"/> representing the result of the operation. </returns>
     public async Task<bool> Handle(SetFileExistsByNameQuery request, CancellationToken cancellationToken)
     {
         var sourceSets = await dbContext.SourceSets.Select(s => new SourceSetHeader(s)).ToListAsync(cancellationToken);
