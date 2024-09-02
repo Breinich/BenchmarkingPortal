348556553afd6f363dfb6059d5a5b78ec51e7635
Breinich <vencel@bajnok.hu>
2024-09-02 19:03:55 +0200

    🐛 GetSetFileNamesBySourceSetIdQueryHandler: Fix handling of non-existing source set
    * Added exception for when the source set is not found in the database
    * Updated Handle method to throw an ArgumentException when the source set is not found
    * Modified the query to filter by Id instead of Name, as Id is the primary key in the SourceSets table.

--- BenchmarkingPortal.Bll/Features/SetFile/QueryHandlers/GetSetFileNamesBySourceSetIdQueryHandler.cs
+++ BenchmarkingPortal.Bll/Features/SetFile/QueryHandlers/GetSetFileNamesBySourceSetIdQueryHandler.cs
@@ -8,8 +8,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BenchmarkingPortal.Bll.Features.SetFile.QueryHandlers;
 
+/// <summary>
+/// Get set file names by source set id
+/// </summary>
+/// <param name="context"></param>
+/// <param name="pathConfigs"></param>
 public class GetSetFileNamesBySourceSetIdQueryHandler(BenchmarkingDbContext context, PathConfigs pathConfigs) : IRequestHandler<GetSetFileNamesBySourceSetIdQuery, IEnumerable<string>>
 {
+    
+    /// <summary>
+    /// Handle get set file names by source set id
+    /// </summary>
+    /// <param name="request"> Get set file names by source set id query </param>
+    /// <param name="cancellationToken"> Cancellation token </param>
+    /// <returns> Set file names </returns>
+    /// <exception cref="ArgumentException"> Throws when source set not found </exception>
     public async Task<IEnumerable<string>> Handle(GetSetFileNamesBySourceSetIdQuery request, CancellationToken cancellationToken)
     {
         var sourceSet = await context.SourceSets.Where(s => s.Id == request.SourceSetId)
