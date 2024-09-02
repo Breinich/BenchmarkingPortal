48389ed82d88e63411245d46c6b7146d1d25f644
Breinich <vencel@bajnok.hu>
2024-09-02 20:07:02 +0200

    🐛 (BenchmarkingPortal.Bll/Features/User/CommandHandlers/DeleteUserCommandHandler.cs): Fix a bug in DeleteUserCommandHandler by updating the constructor to accept UserManager and BenchmarkingDbContext as parameters, and update the code inside Handle method to use the new instance of UserManager instead of the old one. This change ensures that the correct instance of UserManager is used throughout the class, improving the overall consistency and maintainability of the code.

--- BenchmarkingPortal.Bll/Features/User/CommandHandlers/DeleteUserCommandHandler.cs
+++ BenchmarkingPortal.Bll/Features/User/CommandHandlers/DeleteUserCommandHandler.cs
@@ -10,25 +10,23 @@ namespace BenchmarkingPortal.Bll.Features.User.CommandHandlers;
 /// <summary>
 /// Handler for <see cref="DeleteUserCommand"/>
 /// </summary>
-public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand>
+public class DeleteUserCommandHandler(UserManager<Dal.Entities.User> userManager, BenchmarkingDbContext context)
+    : IRequestHandler<DeleteUserCommand>
 {
-    private readonly BenchmarkingDbContext _context;
-    private readonly UserManager<Dal.Entities.User> _userManager;
-
-    public DeleteUserCommandHandler(UserManager<Dal.Entities.User> userManager, BenchmarkingDbContext context)
-    {
-        _userManager = userManager;
-        _context = context;
-    }
-
-
+    
+    /// <summary>
+    /// Deletes a user from the database
+    /// </summary>
+    /// <param name="request"> <see cref="DeleteUserCommand"/> </param>
+    /// <param name="cancellationToken"> <see cref="CancellationToken"/> </param>
+    /// <exception cref="ArgumentException"> If the invoker does not have the privilege to delete a user </exception>
     public async Task Handle(DeleteUserCommand request, CancellationToken cancellationToken)
     {
-        var invoker = await _userManager.FindByNameAsync(request.InvokerName);
+        var invoker = await userManager.FindByNameAsync(request.InvokerName);
 
-        if (invoker != null && await _userManager.IsInRoleAsync(invoker, Roles.Admin))
+        if (invoker != null && await userManager.IsInRoleAsync(invoker, Roles.Admin))
         {
-            var user = await _context.Users.Include(u => u.Executables).Include(u => u.SetFiles)
+            var user = await context.Users.Include(u => u.Executables).Include(u => u.SetFiles)
                            .Include(u => u.Benchmarks).Where(u => u.UserName == request.UserName)
                            .FirstOrDefaultAsync(cancellationToken) ??
                        throw new ArgumentException(ExceptionMessage<Dal.Entities.User>.ObjectNotFound);
@@ -39,9 +37,9 @@ public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand>
 
             foreach (var benchmark in user.Benchmarks) benchmark.User = invoker;
 
-            await _context.SaveChangesAsync(cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
 
-            await _userManager.DeleteAsync(user);
+            await userManager.DeleteAsync(user);
         }
         else
         {
