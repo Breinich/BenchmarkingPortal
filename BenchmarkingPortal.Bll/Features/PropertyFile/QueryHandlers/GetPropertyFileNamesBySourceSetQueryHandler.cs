504158d721b9064477deaaaf8b242098f9cd8200
Breinich <vencel@bajnok.hu>
2024-09-06 01:37:49 +0200

    📝 (BenchmarkingPortal.Bll/Features/PropertyFile/QueryHandlers/GetPropertyFileNamesBySourceSetQueryHandler.cs): Update GetPropertyFileNamesBySourceSetQueryHandler to use PathConfigs service for constructing the property files path
    The change was made to improve the code maintainability and readability by using the PathConfigs service to construct the property files path instead of hardcoding it in the handler. This allows for easier configuration of the working directory, source set directory, and user-specific directories when retrieving the property files.

--- BenchmarkingPortal.Bll/Features/PropertyFile/QueryHandlers/GetPropertyFileNamesBySourceSetQueryHandler.cs
+++ BenchmarkingPortal.Bll/Features/PropertyFile/QueryHandlers/GetPropertyFileNamesBySourceSetQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using BenchmarkingPortal.Bll.Exceptions;
 using BenchmarkingPortal.Bll.Features.PropertyFile.Queries;
+using BenchmarkingPortal.Bll.Services;
 using BenchmarkingPortal.Dal;
 using BenchmarkingPortal.Dal.Dtos;
 using MediatR;
@@ -11,19 +12,21 @@ namespace BenchmarkingPortal.Bll.Features.PropertyFile.QueryHandlers;
 /// Handler for <see cref="GetPropertyFileNamesBySourceSetQuery"/>
 /// </summary>
 // ReSharper disable once UnusedType.Global
-public class GetPropertyFileNamesBySourceSetQueryHandler(BenchmarkingDbContext context)
+public class GetPropertyFileNamesBySourceSetQueryHandler(BenchmarkingDbContext context, PathConfigs pathConfigs)
     : IRequestHandler<GetPropertyFileNamesBySourceSetQuery, IEnumerable<string?>>
 {
     public async Task<IEnumerable<string?>> Handle(GetPropertyFileNamesBySourceSetQuery request, CancellationToken cancellationToken)
     {
-        var propertyRoot = await context.SourceSets
+        var sourceSet = await context.SourceSets
             .Where(x => x.Id == request.SourceSetId)
-            .Select(x => new SourceSetHeader(x).PropertyFilesPath)
-            .FirstOrDefaultAsync(cancellationToken);
-        
-        if (string.IsNullOrWhiteSpace(propertyRoot))
-            throw new ArgumentException(ExceptionMessage<Dal.Entities.SourceSet>.ObjectNotFound);
+            .Select(x => new SourceSetHeader(x))
+            .FirstOrDefaultAsync(cancellationToken) 
+                        ?? throw new ArgumentException(ExceptionMessage<Dal.Entities.SourceSet>.ObjectNotFound);
 
-        return [.. Directory.GetFiles(propertyRoot, "*.properties", SearchOption.TopDirectoryOnly)];
+        return [.. Directory.GetFiles(Path.Join(
+                pathConfigs.WorkingDir, 
+                sourceSet.UserName, 
+                pathConfigs.SourceSetDir, 
+                sourceSet.PropertyFilesPath), "*.prp", SearchOption.TopDirectoryOnly)];
     }
 }
